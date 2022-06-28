551ad9d9e172fb102ef7cfa9d2a6cbda6375f2a9
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-06-28 16:03:28 -0600

    implemented financial charts fluent API

---
+++ Reveal.Sdk.Dom/Visualizations/Extensions/FinancialVisualizationBaseExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class FinancialVisualizationBaseExtensions
+    {
+        public static T AddOpen<T>(this T visualization, string openField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.AddOpen(new SummarizationValueField(openField));
+            return visualization;
+        }
+
+        public static T AddOpen<T>(this T visualization, SummarizationValueField openField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.Open.Add(new MeasureColumnSpec() { SummarizationField = openField });
+            return visualization;
+        }
+
+        public static T AddHigh<T>(this T visualization, string highField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.AddHigh(new SummarizationValueField(highField));
+            return visualization;
+        }
+
+        public static T AddHigh<T>(this T visualization, SummarizationValueField highField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.High.Add(new MeasureColumnSpec() { SummarizationField = highField });
+            return visualization;
+        }
+
+        public static T AddLow<T>(this T visualization, string lowField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.AddLow(new SummarizationValueField(lowField));
+            return visualization;
+        }
+
+        public static T AddLow<T>(this T visualization, SummarizationValueField lowField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.Low.Add(new MeasureColumnSpec() { SummarizationField = lowField });
+            return visualization;
+        }
+
+        public static T AddClose<T>(this T visualization, string closeField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.AddClose(new SummarizationValueField(closeField));
+            return visualization;
+        }
+
+        public static T AddClose<T>(this T visualization, SummarizationValueField closeField)
+            where T : FinancialVisualizationBase<ChartVisualizationSettings>
+        {
+            visualization.Close.Add(new MeasureColumnSpec() { SummarizationField = closeField });
+            return visualization;
+        }
+    }
+}
--- Reveal.Sdk.Dom/Visualizations/FinancialVisualizationBase.cs
+++ Reveal.Sdk.Dom/Visualizations/FinancialVisualizationBase.cs
@@ -2,18 +2,32 @@
 using Reveal.Sdk.Dom.Data;
 using Reveal.Sdk.Dom.Visualizations.Settings;
 using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
 
 namespace Reveal.Sdk.Dom.Visualizations
 {
-    public abstract class FinancialVisualizationBase<TSettings> : Visualization<TSettings>
+    public abstract class FinancialVisualizationBase<TSettings> : Visualization<TSettings>, ILabels
         where TSettings : ChartVisualizationSettings, new()
     {
         protected FinancialVisualizationBase(DataSourceItem dataSourceItem) : base(dataSourceItem) { }
         protected FinancialVisualizationBase(string title, DataSourceItem dataSourceItem) : base(title, dataSourceItem) { }
 
-        //todo: implement
-
         [JsonProperty(Order = 7)]
         FinancialVisualizationDataSpec VisualizationDataSpec { get; set; } = new FinancialVisualizationDataSpec();
+
+        [JsonIgnore]
+        public List<DimensionColumnSpec> Labels { get { return VisualizationDataSpec.Rows; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> Open { get { return VisualizationDataSpec.Open; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> High { get { return VisualizationDataSpec.High; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> Low { get { return VisualizationDataSpec.Low; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> Close { get { return VisualizationDataSpec.Close; } }
     }
 }
