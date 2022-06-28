120b06edf7128edc47bbc46ea0dfcd87436a608e
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-06-27 18:36:18 -0600

    added documentation to the dashboard document

--- Reveal.Sdk.Dom/DashboardDocument.cs
+++ Reveal.Sdk.Dom/DashboardDocument.cs
@@ -19,37 +19,89 @@ namespace Reveal.Sdk.Dom
             Title = title;
         }
 
+        /// <summary>
+        /// Gets or sets the title of the dashboard.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the desctiption of the dashboard
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the theme the dashboard will apply to all visualizations.
+        /// </summary>
         [JsonProperty("ThemeName")]
         public string Theme { get; set; }
 
+        /// <summary>
+        /// Gets the name of the API that created the dashboard.
+        /// </summary>
         [JsonProperty]
         public string CreatedWith { get; private set; } = GlobalConstants.DashboardDocument.CreatedWith;
 
+        /// <summary>
+        /// Gets the name of the API that last saved the dashboard.
+        /// </summary>
         [JsonProperty]
         public string SavedWith { get; internal set; } = string.Empty;
 
+        /// <summary>
+        /// Gets a value which represents the format of the dashboard.
+        /// </summary>
         [JsonProperty]
         public int FormatVersion { get; private set; }
+
+        /// <summary>
+        /// Gets or sets whether the RevealView displaying the dashboard will automatically layout visualizations, or use an absolute layout controlled by each visualization's ColumnSpan and RowSpan properties. True by default.
+        /// </summary>
         public bool UseAutoLayout { get; set; } = true;
+
+        /// <summary>
+        /// TODO:
+        /// </summary>
         public int? AutoRefreshInterval { get; set; }
+
+        /// <summary>
+        /// TODO:
+        /// </summary>
         public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// TODO:
+        /// </summary>
         public string Tags { get; set; }
 
+        /// <summary>
+        /// Gets the collection of data sources available for creating visualizations.
+        /// </summary>
         [JsonProperty]
         public List<DataSource> DataSources { get; internal set; } = new List<DataSource>();
 
+        /// <summary>
+        /// Gets the collection of dashboard filters that can bound to any visualization using the visualization's FilterBindings property.
+        /// </summary>
         [JsonProperty("GlobalFilters")]
         public List<DashboardFilter> Filters { get; internal set; } = new List<DashboardFilter>();
 
+        /// <summary>
+        /// TODO: implement
+        /// </summary>
         [JsonProperty]
         internal List<GlobalVariable> GlobalVariables { get; set; } = new List<GlobalVariable>();
 
+        /// <summary>
+        /// Gets the collection of visualizations that are displayed in the dashboard.
+        /// </summary>
         [JsonProperty("Widgets")]
         public List<IVisualization> Visualizations { get; internal set; } = new List<IVisualization>();
 
+        /// <summary>
+        /// Creates a DashboardDocument from a dashboard file (.rdash).
+        /// </summary>
+        /// <param name="filePath">The file path to the dashboard file (.rdash).</param>
+        /// <returns>The DashbardDocument representing the contents of the loaded .rdash file</returns>
         public static DashboardDocument Load(string filePath)
         {
             return RdashSerializer.Load(filePath);
@@ -61,7 +113,7 @@ namespace Reveal.Sdk.Dom
         /// <param name="filePath">The file path to save the Dashboard (must include the .rdash extensions)</param>
         public void Save(string filePath)
         {
-            RdashSerializer.Save(this, filePath);
+            RdashSerializer.Save(this, filePath);            
         }
 
         /// <summary>
