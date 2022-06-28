313f8518b57fc9844c3a4fa9e89215ec8a01a3f7
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-06-28 16:42:59 -0600

    added scatter properties

--- Reveal.Sdk.Dom/Visualizations/ScatterVisualization.cs
+++ Reveal.Sdk.Dom/Visualizations/ScatterVisualization.cs
@@ -2,18 +2,26 @@
 using Reveal.Sdk.Dom.Data;
 using Reveal.Sdk.Dom.Visualizations.Settings;
 using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
 
 namespace Reveal.Sdk.Dom.Visualizations
 {
-    public class ScatterVisualization : Visualization<ScatterVisualizationSettings>
+    public class ScatterVisualization : Visualization<ScatterVisualizationSettings>, ILabels, IAxis
     {
         internal ScatterVisualization() : this(null) { }
         public ScatterVisualization(DataSourceItem dataSourceItem) : base(dataSourceItem) { }
         public ScatterVisualization(string title, DataSourceItem dataSourceItem) : base(title, dataSourceItem) { }
 
-        //todo: implement
-
         [JsonProperty(Order = 7)]
         ScatterVisualizationDataSpec VisualizationDataSpec { get; set; } = new ScatterVisualizationDataSpec();
+
+        [JsonIgnore]
+        public List<DimensionColumnSpec> Labels { get { return VisualizationDataSpec.Rows; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> XAxis { get { return VisualizationDataSpec.XAxis; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> YAxis { get { return VisualizationDataSpec.YAxis; } }
     }
 }
