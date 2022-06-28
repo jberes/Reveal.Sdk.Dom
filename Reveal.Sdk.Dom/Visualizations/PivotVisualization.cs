84857d718cad06615b18a879da80e6b7eb216269
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-06-28 16:18:10 -0600

    removed unused pivot base class

--- Reveal.Sdk.Dom/Visualizations/PivotVisualization.cs
+++ Reveal.Sdk.Dom/Visualizations/PivotVisualization.cs
@@ -1,12 +1,34 @@
-﻿using Reveal.Sdk.Dom.Data;
+﻿using Newtonsoft.Json;
+using Reveal.Sdk.Dom.Data;
 using Reveal.Sdk.Dom.Visualizations.Settings;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
 
 namespace Reveal.Sdk.Dom.Visualizations
 {
-    public class PivotVisualization : PivotVisualizationBase<PivotVisualizationSettings>
+    public class PivotVisualization : Visualization<PivotVisualizationSettings>, IValues, IRows, IColumns
     {
         internal PivotVisualization() : this(null) { }
         public PivotVisualization(DataSourceItem dataSourceItem) : base(string.Empty, dataSourceItem) { }
         public PivotVisualization(string title, DataSourceItem dataSourceItem) : base(title, dataSourceItem) { }
+
+        [JsonIgnore]
+        public List<DimensionColumnSpec> Columns { get { return VisualizationDataSpec.Columns; } }
+
+        [JsonIgnore]
+        public List<DimensionColumnSpec> Rows { get { return VisualizationDataSpec.Rows; } }
+
+        [JsonIgnore]
+        public List<MeasureColumnSpec> Values { get { return VisualizationDataSpec.Values; } }
+
+        [JsonIgnore]
+        public bool ShowGrandTotals
+        {
+            get { return VisualizationDataSpec.ShowGrandTotals; }
+            set { VisualizationDataSpec.ShowGrandTotals = value; }
+        }
+
+        [JsonProperty(Order = 7)]
+        PivotVisualizationDataSpec VisualizationDataSpec { get; set; } = new PivotVisualizationDataSpec();
     }
 }
