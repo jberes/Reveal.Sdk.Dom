db6550361a3e6f6da2a33eeb6842c2260e14bfe6
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-07-15 12:18:22 -0600

    added configure settings method for step area and line charts

---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/Visualizations/StepAreaChartVisualizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+using System;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class StepAreaChartVisualizationExtensions
+    {
+        public static StepAreaChartVisualization ConfigureSettings(this StepAreaChartVisualization visualization, Action<StepAreaChartVisualizationSettings> settings)
+        {
+            return visualization.ConfigureSettings<StepAreaChartVisualization, StepAreaChartVisualizationSettings>(settings);
+        }
+    }
+}
---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/Visualizations/StepLineChartVisualizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+using System;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class StepLineChartVisualizationExtensions
+    {
+        public static StepLineChartVisualization ConfigureSettings(this StepLineChartVisualization visualization, Action<StepLineChartVisualizationSettings> settings)
+        {
+            return visualization.ConfigureSettings<StepLineChartVisualization, StepLineChartVisualizationSettings>(settings);
+        }
+    }
+}
