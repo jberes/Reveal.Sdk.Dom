8b9a4d43b35dc90d19abb3b2158517a155619473
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-07-15 12:12:49 -0600

    added extension methods to configure stacked series settings

---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/Visualizations/StackedAreaChartVisualizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+using System;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class StackedAreaChartVisualizationExtensions
+    {
+        public static StackedAreaChartVisualization ConfigureSettings(this StackedAreaChartVisualization visualization, Action<StackedAreaChartVisualizationSettings> settings)
+        {
+            return visualization.ConfigureSettings<StackedAreaChartVisualization, StackedAreaChartVisualizationSettings>(settings);
+        }
+    }
+}
---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/Visualizations/StackedBarChartVisualizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+using System;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class StackedBarChartVisualizationExtensions
+    {
+        public static StackedBarChartVisualization ConfigureSettings(this StackedBarChartVisualization visualization, Action<StackedBarChartVisualizationSettings> settings)
+        {
+            return visualization.ConfigureSettings<StackedBarChartVisualization, StackedBarChartVisualizationSettings>(settings);
+        }
+    }
+}
