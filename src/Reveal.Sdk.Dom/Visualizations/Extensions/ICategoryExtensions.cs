1799a43f1fcde8f6099cd9ac67e85498473a54d7
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-07-20 13:42:26 -0600

    added icategory extensions

---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/ICategoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class ICategoryExtensions
+    {
+        public static T SetCategory<T>(this T visualization, string field)
+            where T : ICategory
+        {
+            return visualization.SetCategory(new SummarizationRegularField(field));
+        }
+
+        public static T SetCategory<T>(this T visualization, SummarizationDimensionField field)
+            where T : ICategory
+        {
+            visualization.Category = new DimensionColumnSpec() { SummarizationField = field };
+            return visualization;
+        }
+    }
+}
