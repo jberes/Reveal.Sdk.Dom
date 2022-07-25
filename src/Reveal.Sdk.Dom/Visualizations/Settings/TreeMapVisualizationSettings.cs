6284f064e9aef8d0f6e0ac62a63648481cd20a88
Brian Lagunas <brianlagunas@users.noreply.github.com>
2022-07-25 11:12:10 -0600

    refactored treemap settings

--- src/Reveal.Sdk.Dom/Visualizations/Settings/TreeMapVisualizationSettings.cs
+++ src/Reveal.Sdk.Dom/Visualizations/Settings/TreeMapVisualizationSettings.cs
@@ -1,65 +1,26 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using Reveal.Sdk.Dom.Core.Constants;
 
 namespace Reveal.Sdk.Dom.Visualizations.Settings
 {
-	//todo: clean up and move classes out
     public sealed class TreeMapVisualizationSettings : VisualizationSettings
     {
-		[JsonConverter(typeof(StringEnumConverter))]
-		public DashboardTreeMapLayoutEnumType Layout { get; set; } = DashboardTreeMapLayoutEnumType.Squarified;
-		
-		[JsonProperty]
-		internal int? BrushOffsetIndex { get; set; } = -1;
-
-		public bool ShowValues { get; set; } = true;
-		public bool ShowLegend { get; set; } = true;
-
-		[JsonConverter(typeof(StringEnumConverter))]
-		public DashboardTreeMapColorType ColorAs { get; set; } = DashboardTreeMapColorType.SingleColor;
-
-		[JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Replace)]
-		public TreeMapBound MinBound { get; set; } = new TreeMapBound() { Color = DashboardTreeMapBoundColorType.Red };
-
-		[JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Replace)]
-		public TreeMapBound MaxBound { get; set; } = new TreeMapBound() { Color = DashboardTreeMapBoundColorType.Green };
-
-        [JsonProperty]
-		internal string LabelColumnName { get; set; }
-
-		[JsonProperty]
-		internal string ValueColumnName { get; set; }
-
-        public TreeMapVisualizationSettings()
-        {
+		public TreeMapVisualizationSettings()
+		{
 			SchemaTypeName = SchemaTypeNames.TreeMapVisualizationSettingsType;
 			VisualizationType = VisualizationTypes.TREE_MAP;
 		}
-	}
 
-	public enum DashboardTreeMapLayoutEnumType
-	{
-		Squarified,
-		SliceAndDice,
-		Strip
-	}
+		/// <summary>
+		/// Gets or sets the color index for the visualization's starting color. A zero-based index is used to set colors instead of a color name.
+		/// For example, an index of 5 would be the 6th color in the color scheme regardless of the theme colors being used.
+		/// </summary>
+		[JsonProperty("BrushOffsetIndex")]
+		public int? StartColorIndex { get; set; }
 
-	public enum DashboardTreeMapColorType
-	{
-		SingleColor,
-		Gradient
-	}
-
-	public sealed class TreeMapBound
-    {
-		public DashboardTreeMapBoundColorType Color { get; set; } = DashboardTreeMapBoundColorType.Green;
-		public double? Value { get; set; }
-	}
-
-	public enum DashboardTreeMapBoundColorType
-	{
-		Green,
-		Red
+		/// <summary>
+		/// Gets or sets whether to show labels that display information about the categories and values.
+		/// </summary>
+		public bool ShowValues { get; set; } = true;
 	}
 }
