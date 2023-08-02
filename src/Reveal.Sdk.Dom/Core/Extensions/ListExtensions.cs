4b754686e2a2c95e7067d36b3ad0d81b8ab046e2
Brian Lagunas <brianlagunas@users.noreply.github.com>
2023-08-02 11:09:03 -0600

    added extension methods to remove visualizations by name and id

---
+++ src/Reveal.Sdk.Dom/Core/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Reveal.Sdk.Dom.Visualizations;
+using System.Collections.Generic;
+
+namespace Reveal.Sdk.Dom
+{
+    public static class ListExtensions
+    {
+        public static List<IVisualization> RemoveById(this List<IVisualization> list, string id)
+        {
+            list.RemoveAll(v => v.Id == id);
+            return list;
+        }
+
+        public static List<IVisualization> RemoveByTitle(this List<IVisualization> list, string title)
+        {
+            list.RemoveAll(v => v.Title.Trim().ToLower() == title.Trim().ToLower());
+            return list;
+        }
+    }
+}
