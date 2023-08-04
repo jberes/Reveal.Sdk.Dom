b704aa2da54be54d614e313ba2763cf988b58327
Brian Lagunas <brianlagunas@users.noreply.github.com>
2023-08-04 17:33:59 -0600

    add validation for manually added data source items

--- src/Reveal.Sdk.Dom/Core/Utilities/RdashDocumentValidator.cs
+++ src/Reveal.Sdk.Dom/Core/Utilities/RdashDocumentValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Reveal.Sdk.Dom.Data;
 using Reveal.Sdk.Dom.Visualizations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,22 +18,46 @@ namespace Reveal.Sdk.Dom.Core.Utilities
             Dictionary<string, DataSource> dataSources = new Dictionary<string, DataSource>();
             foreach (var visualization in document.Visualizations)
             {
+                if (visualization is TextBoxVisualization) //TextBoxVisualization does not require a data source so there is no need to fix it
+                    continue;
+
                 if (visualization is IDataDefinitionProvider<DataDefinitionBase> iddp)
                 {
                     var dsi = iddp.DataDefinition?.DataSourceItem;
-                    if (dsi != null)
+                    if (dsi == null) 
+                        throw new Exception($"Data source item for visualization {visualization.Title} is null.");
+
+                    if (dsi.DataSource != null)
                     {
-                        if (dsi.DataSource != null && !dataSources.ContainsKey(dsi.DataSource.Id))
+                        if (!dataSources.ContainsKey(dsi.DataSource.Id))
                             dataSources.Add(dsi.DataSource.Id, dsi.DataSource);
 
                         if (dsi.ResourceItemDataSource != null && !dataSources.ContainsKey(dsi.ResourceItemDataSource.Id))
                             dataSources.Add(dsi.ResourceItemDataSource.Id, dsi.ResourceItemDataSource);
                     }
-                }                
+                    else
+                    {
+                        ValidateManuallyAddedDataSourceItem(document, dsi);
+                    }
+                }
             }
 
             var allDataSources = document.DataSources?.Union(dataSources.Values.ToArray());
             document.DataSources = allDataSources?.ToList();
+        }
+
+        static void ValidateManuallyAddedDataSourceItem(RdashDocument document, DataSourceItem dsi)
+        {
+            var ds = document.DataSources?.FirstOrDefault(x => x.Id == dsi.DataSourceId);
+            if (ds == null)
+                throw new Exception($"Data source with id {dsi.DataSourceId} not found in the RdashDocument.DataSources collection.");
+
+            if (dsi.ResourceItem != null)
+            {
+                var rds = document.DataSources?.FirstOrDefault(x => x.Id == dsi.ResourceItem.DataSourceId);
+                if (rds == null)
+                    throw new Exception($"ResourceItem with Data source id {dsi.ResourceItem.DataSourceId} not found in the RdashDocument.DataSources collection.");
+            }
         }
     }
 }
