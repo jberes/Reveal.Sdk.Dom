5c625fa1dafba0a15308d7588a7123289ef24fea
Henry <hnguyen@infragistics.com>
2024-11-13 16:13:20 +0700

    apply tests for GA4 Data source item

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/GoogleAnalytics4DataSourceItemFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reveal.Sdk.Dom.Core.Extensions;
+using Reveal.Sdk.Dom.Data;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
+{
+    public class GoogleAnalytics4DataSourceItemFixture
+    {
+        [Fact]
+        public void Constructor_SetsTitleAndDatasource_AsInputs()
+        {
+            // Arrange
+            var dataSource = new GoogleAnalytics4DataSource();
+
+            // Act
+            var dataSourceItem = new GoogleAnalytics4DataSourceItem("Test title", dataSource);
+
+            // Assert
+            Assert.Equal("Test title", dataSourceItem.Title);
+            Assert.Equal(dataSource, dataSourceItem.DataSource);
+        }
+
+        [Fact]
+        public void AccountId_GetAndSet_ShouldSetProperties()
+        {
+            // Arrange
+            var dataSource = new GoogleAnalytics4DataSource();
+            var dataSourceItem = new GoogleAnalytics4DataSourceItem("Test", dataSource);
+
+            // Act
+            dataSourceItem.AccountId = "TestAccountId";
+
+            // Assert
+            Assert.Equal("TestAccountId", dataSourceItem.AccountId);
+            Assert.Equal("TestAccountId", dataSourceItem.Properties.GetValue<string>("AccountId"));
+        }
+
+        [Fact]
+        public void PropertyId_GetAndSet_ShouldSetProperties()
+        {
+            // Arrange
+            var dataSource = new GoogleAnalytics4DataSource();
+            var dataSourceItem = new GoogleAnalytics4DataSourceItem("Test", dataSource);
+
+            // Act
+            dataSourceItem.PropertyId = "TestPropertyId";
+
+            // Assert
+            Assert.Equal("TestPropertyId", dataSourceItem.PropertyId);
+            Assert.Equal("TestPropertyId", dataSourceItem.Properties.GetValue<string>("PropertyId"));
+        }
+    }
+}
