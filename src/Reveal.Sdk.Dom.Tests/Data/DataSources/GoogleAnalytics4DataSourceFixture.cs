8804554d3f48168d350a15e8ddb9a1fc3dd5c9b0
Henry <hnguyen@infragistics.com>
2024-11-13 16:13:06 +0700

    apply test for GA4 datasource

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/GoogleAnalytics4DataSourceFixture.cs
@@ -0,0 +1,18 @@
+﻿using Reveal.Sdk.Dom.Data;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSources
+{
+    public class GoogleAnalytics4DataSourceFixture
+    {
+        [Fact]
+        public void Constructor_SetProvider_ToMicrosoftSqlServer()
+        {
+            // Act
+            var dataSource = new GoogleAnalytics4DataSource();
+
+            // Assert
+            Assert.Equal(DataSourceProvider.GoogleAnalytics4, dataSource.Provider);
+        }
+    }
+}
