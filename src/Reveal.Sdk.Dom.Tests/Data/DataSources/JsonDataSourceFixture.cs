03c75c023bf6db588caf3fb7afbc972b0d853327
Henry <hnguyen@infragistics.com>
2024-11-14 16:57:20 +0700

    apply basic local file tests

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/JsonDataSourceFixture.cs
@@ -0,0 +1,25 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSources
+{
+    public class JsonDataSourceFixture
+    {
+        [Fact]
+        public void Constructor_SetProviderToGoogleAnalytics4_WhenConstructed()
+        {
+            // Act
+            var dataSource = new JsonDataSource();
+
+            // Assert
+            Assert.Equal(DataSourceProvider.JSON, dataSource.Provider);
+            Assert.Equal(DataSourceIds.JSON, dataSource.Id);
+        }
+    }
+}
---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/LocalFileDataSourceFixture.cs
@@ -0,0 +1,25 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSources
+{
+    public class LocalFileDataSourceFixture
+    {
+        [Fact]
+        public void Constructor_SetProviderToGoogleAnalytics4_WhenConstructed()
+        {
+            // Act
+            var dataSource = new LocalFileDataSource();
+
+            // Assert
+            Assert.Equal(DataSourceProvider.LocalFile, dataSource.Provider);
+            Assert.Equal(DataSourceIds.LOCALFILE, dataSource.Id);
+        }
+    }
+}
