2e996de6b86fa97144c7017b5e8476f113a95690
Viktor <VLe@infragistics.com>
2024-11-15 16:54:10 +0700

    Add unit tests for Host Data Source

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/HostDataSourceFixture.cs
@@ -0,0 +1,37 @@
+﻿using Reveal.Sdk.Dom.Data;
+using System;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSources
+{
+    public class HostDataSourceFixture
+    {
+        [Fact]
+        public void GetHost_ReturnSameValue_WithSetValue()
+        {
+            // Arrange
+            var hostDataSource = new HostDataSource();
+            var expectedHost = "test-host";
+
+            // Act
+            hostDataSource.Host = expectedHost;
+
+            // Assert
+            Assert.Equal(expectedHost, hostDataSource.Host);
+        }
+
+        [Fact]  
+        public void GetPort_ReturnSameValue_WithSetPort()
+        {
+            // Arrange
+            var hostDataSource = new HostDataSource();
+            var expectedPort = "port";
+
+            // Act
+            hostDataSource.Port = expectedPort;
+
+            // Assert
+            Assert.Equal(expectedPort, hostDataSource.Port);
+        }
+    }
+}
