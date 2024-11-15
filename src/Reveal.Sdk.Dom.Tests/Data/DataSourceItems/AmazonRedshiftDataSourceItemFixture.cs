891c8a2469da3ca808fc4a74ba9cd24f42591c07
Phat Vo <recca5p@gmail.com>
2024-11-15 16:28:36 +0700

    clean up unit tests in the items datasource

--- src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/AmazonRedshiftDataSourceItemFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/AmazonRedshiftDataSourceItemFixture.cs
@@ -13,57 +13,9 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             var dataSource = new AmazonRedshiftDataSource();
             var item = new AmazonRedshiftDataSourceItem(title, dataSource);
 
-            // Act
-            var actualTitle = item.Title;
-            var actualDataSource = item.DataSource;
-
             // Assert
-            Assert.Equal(title, actualTitle);
-            Assert.Equal(dataSource, actualDataSource);
-        }
-
-        [Fact]
-        public void Constructor_SetsTitleAndDataSource_NullDataSource()
-        {
-            // Arrange
-            string title = "Test Item";
-            DataSource dataSource = null;
-            var item = new AmazonRedshiftDataSourceItem(title, dataSource);
-
-            // Act
-            var actualTitle = item.Title;
-            var actualDataSource = item.DataSource;
-
-            // Assert
-            Assert.Equal(title, actualTitle);
-        }
-
-        [Fact]
-        public void Constructor_SetsTitle_EmptyTitle()
-        {
-            // Arrange
-            string title = string.Empty;
-            var dataSource = new AmazonRedshiftDataSource();
-            var item = new AmazonRedshiftDataSourceItem(title, dataSource);
-
-            // Act
-            var actualTitle = item.Title;
-
-            // Assert
-            Assert.Equal(title, actualTitle);
-        }
-
-        [Fact]
-        public void HasAsset_ReturnsFalse_Always()
-        {
-            // Arrange
-            var item = new AmazonRedshiftDataSourceItem("Test Item", new AmazonRedshiftDataSource());
-
-            // Act
-            var hasAsset = item.HasAsset;
-
-            // Assert
-            Assert.False(hasAsset);
+            Assert.Equal(title, item.Title);
+            Assert.Equal(dataSource, item.DataSource);
         }
     }
 }
--- src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/AmazonS3DataSourceItemFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/AmazonS3DataSourceItemFixture.cs
@@ -14,59 +14,9 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             var dataSource = new AmazonS3DataSource();
             var item = new AmazonS3DataSourceItem(title, dataSource);
 
-            // Act
-            var actualTitle = item.Title;
-            var actualDataSource = item.DataSource;
-
             // Assert
-            Assert.Equal(title, actualTitle);
-            Assert.Equal(dataSource, actualDataSource);
-        }
-
-        [Fact]
-        public void Constructor_SetsTitleAndDataSource_NullDataSource()
-        {
-            // Arrange
-            string title = "Test Item";
-            DataSource dataSource = null;
-            var item = new AmazonS3DataSourceItem(title, dataSource);
-
-            // Act
-            var actualTitle = item.Title;
-            var actualDataSource = item.DataSource;
-
-            // Assert
-            Assert.Equal(title, actualTitle);
-        }
-
-        [Fact]
-        public void Constructor_SetsTitle_EmptyTitle()
-        {
-            // Arrange
-            string title = string.Empty;
-            var dataSource = new AmazonS3DataSource();
-            var item = new AmazonS3DataSourceItem(title, dataSource);
-
-            // Act
-            var actualTitle = item.Title;
-            var actualDataSource = item.DataSource;
-
-            // Assert
-            Assert.Equal(title, actualTitle);
-            Assert.Equal(dataSource, actualDataSource);
-        }
-
-        [Fact]
-        public void HasAsset_ReturnsFalse_Always()
-        {
-            // Arrange
-            var item = new AmazonS3DataSourceItem("Test Item", new AmazonS3DataSource());
-
-            // Act
-            var hasAsset = item.HasAsset;
-
-            // Assert
-            Assert.False(hasAsset);
+            Assert.Equal(title, item.Title);
+            Assert.Equal(dataSource, item.DataSource);
         }
 
         [Fact]
@@ -79,9 +29,11 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             // Act
             item.Path = expectedPath;
             var actualPath = item.Path;
+            var actualPropertyPath = item.Properties.GetValue<string>("Path");
 
             // Assert
             Assert.Equal(expectedPath, actualPath);
+            Assert.Equal(expectedPath, actualPropertyPath);
         }
 
         [Fact]
@@ -98,21 +50,6 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             // Assert
             Assert.Null(actualPath);
             Assert.Null(actualPropertyPath);
-        }
-
-        [Fact]
-        public void Properties_StoresPathValueCorrectly_ValidPath()
-        {
-            // Arrange
-            var item = new AmazonS3DataSourceItem("Test Item", new AmazonS3DataSource());
-            var expectedPath = "s3://bucket-name/path/to/resource";
-
-            // Act
-            item.Path = expectedPath;
-            var actualPropertyPath = item.Properties.GetValue<string>("Path");
-
-            // Assert
-            Assert.Equal(expectedPath, actualPropertyPath);
         }
     }
 }
