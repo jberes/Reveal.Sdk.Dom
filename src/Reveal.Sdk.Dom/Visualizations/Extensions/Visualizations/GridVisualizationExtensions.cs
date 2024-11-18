9abae49948f0a4ce5a7f1acc322bdbe7668580c4
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2024-11-18 09:28:07 -0700

    added grid paging to grid visualization

---
+++ src/Reveal.Sdk.Dom/Visualizations/Extensions/Visualizations/GridVisualizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Reveal.Sdk.Dom.Visualizations.Settings;
+using System;
+
+namespace Reveal.Sdk.Dom.Visualizations
+{
+    public static class GridVisualizationExtensions
+    {
+        public static GridVisualization ConfigureSettings(this GridVisualization visualization, Action<GridVisualizationSettings> settings)
+        {
+            return visualization.ConfigureSettings<GridVisualization, GridVisualizationSettings>(settings);
+        }
+    }
+}
--- src/Reveal.Sdk.Dom/Visualizations/Settings/GridVisualizationSettings.cs
+++ src/Reveal.Sdk.Dom/Visualizations/Settings/GridVisualizationSettings.cs
@@ -12,10 +12,24 @@ namespace Reveal.Sdk.Dom.Visualizations.Settings
         }
 
         /// <summary>
+        /// Gets or sets whether the grid should have paging enabled. 
+        /// Paging is supported only when <c>ProcessDataOnServer</c> is set to <c>true</c> and is not compatible with Data Blending.
+        /// Supported data sources include: Athena, BigQuery, MySQL, Oracle, PostgreSQL, SQL Server, and SyBase.
+        /// </summary>
+        [JsonProperty("PagedRows")]
+        public bool IsPagingEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the number of rows to display per page.
+        /// </summary>
+        [JsonProperty("PagedRowsSize")]
+        public int PageSize { get; set; } = 50;
+
+        /// <summary>
         /// Gets or sets whether the first column in the grid is fixed.
         /// </summary>
         [JsonIgnore()]
-        public bool FixFirstColumn
+        public bool IsFirstColumnFixed
         {
             get { return Style.FixedLeftColumns; }
             set { Style.FixedLeftColumns = value; }
