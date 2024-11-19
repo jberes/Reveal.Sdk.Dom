36a63f5b66f34f2e89b2f6b2bf39a18fe54f990d
Viktor <VLe@infragistics.com>
2024-11-19 14:57:44 +0700

    Add unit tests for Table Data Source Item

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/TableDataSourceltemFixture.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Data;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
+{
+    public class TableDataSourceItemFixture
+    {
+        [Fact]
+        public void Constructor_CreateTableDataSourceItem_WithTitleAndDataSource()
+        {
+            // Arrange
+            var title = "Title";
+            var dataSource = new DataSource();
+
+            // Act
+            var mock = new Mock<TableDataSourceItem>(title, dataSource) { CallBase = true };
+            var tableDataSourceItem = mock.Object;
+
+            // Assert
+            Assert.Equal(title, tableDataSourceItem.Title);
+            Assert.Equal(dataSource, tableDataSourceItem.DataSource);
+            Assert.Equal(dataSource.Id, tableDataSourceItem.DataSourceId);
+            Assert.Equal(title, tableDataSourceItem.DataSource.Title);
+        }
+
+        [Fact]
+        public void GetTable_ReturnSameValue_WithSetValue()
+        {
+            // Arrange
+            var title = "Title";
+            var dataSource = new DataSource();
+            var mock = new Mock<TableDataSourceItem>(title, dataSource) { CallBase = true };
+            var tableDataSourceItem = mock.Object;
+            var expectedTable = "Test Table";
+
+            // Act
+            tableDataSourceItem.Table = expectedTable;
+
+            // Assert
+            Assert.Equal(expectedTable, tableDataSourceItem.Table);
+        }
+    }
+}
