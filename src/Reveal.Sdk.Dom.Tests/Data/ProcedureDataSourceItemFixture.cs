2b38708d91d02e40f80ef784b809989c1d68263d
Viktor <VLe@infragistics.com>
2024-11-19 16:42:36 +0700

    Add unit tests for Procedure Data source Item

---
+++ src/Reveal.Sdk.Dom.Tests/Data/ProcedureDataSourceItemFixture.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Data;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data
+{
+    public class ProcedureDataSourceItemFixture
+    {
+        [Fact]
+        public void Constructor_CreateProcedureDataSourceItem_WithTitleAndDataSource()
+        {
+            // Arrange
+            var title = "Title";
+            var dataSource = new DataSource();
+
+            // Act
+            var mock = new Mock<ProcedureDataSourceItem>(title, dataSource) { CallBase = true };
+            var procedureDataSourceItem = mock.Object;
+
+            // Assert
+            Assert.Equal(title, procedureDataSourceItem.Title);
+            Assert.Equal(dataSource, procedureDataSourceItem.DataSource);
+            Assert.Equal(dataSource.Id, procedureDataSourceItem.DataSourceId);
+            Assert.Equal(title, procedureDataSourceItem.DataSource.Title);
+        }
+    }
+}
