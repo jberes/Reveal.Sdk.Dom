1a9c9c83c19210660189a2be6ebf4a7c37b2a375
Viktor <VLe@infragistics.com>
2024-11-19 15:18:19 +0700

    Create Process Data Source Unit tests

---
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/ProcessDataSourceFixture.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Core.Extensions;
+using Reveal.Sdk.Dom.Data;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Data.DataSources
+{
+    public class ProcessDataSourceFixture
+    {
+        [Fact]
+        public void Constructor_CreateProcessDataSource_WithoutParameters()
+        {
+            // Arrange
+            var expectedProcessDataOnServerDefaultValue = true;
+            var expectedProcessDataOnServerReadOnly = false;
+
+            // Act
+            var mock = new Mock<ProcessDataSource>();
+            var processDataSource = mock.Object;
+
+            // Assert
+            Assert.Equal(expectedProcessDataOnServerDefaultValue, processDataSource.ProcessDataOnServerDefaultValue);
+            Assert.Equal(expectedProcessDataOnServerReadOnly, processDataSource.ProcessDataOnServerReadOnly);
+        }
+
+        [Fact]
+        public void GetProcessDataOnServerDefaultValue_ReturnSameValue_WithSetValue()
+        {
+            // Arrange
+            var expectedProcessDataOnServerDefaultValue = false;
+            var mock = new Mock<ProcessDataSource>();
+            var processDataSource = mock.Object;
+
+            // Act
+            processDataSource.ProcessDataOnServerDefaultValue = expectedProcessDataOnServerDefaultValue;
+
+            // Assert
+            Assert.Equal(expectedProcessDataOnServerDefaultValue, processDataSource.ProcessDataOnServerDefaultValue);
+            Assert.Equal(expectedProcessDataOnServerDefaultValue, processDataSource.Properties.GetValue<bool>("ServerAggregationDefault"));
+        }
+
+        [Fact]
+        public void GetProcessDataOnServerReadOnly_ReturnSameValue_WithSetValue()
+        {
+            // Arrange
+            var expectedProcessDataOnServerReadOnly = false;
+            var mock = new Mock<ProcessDataSource>();
+            var processDataSource = mock.Object;
+
+            // Act
+            processDataSource.ProcessDataOnServerReadOnly = expectedProcessDataOnServerReadOnly;
+
+            // Assert
+            Assert.Equal(expectedProcessDataOnServerReadOnly, processDataSource.ProcessDataOnServerReadOnly);
+            Assert.Equal(expectedProcessDataOnServerReadOnly, processDataSource.Properties.GetValue<bool>("ServerAggregationReadOnly"));
+        }
+    }
+}
