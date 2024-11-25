a1d7d3098d78b3f79540079bd79fe9a12666b97d
Viktor <VLe@infragistics.com>
2024-11-25 17:13:20 +0700

    Create constructors for Postgres DataSource Item

--- src/Reveal.Sdk.Dom/Data/DataSourceItems/PostgreSqlDataSourceItem.cs
+++ src/Reveal.Sdk.Dom/Data/DataSourceItems/PostgreSqlDataSourceItem.cs
@@ -5,6 +5,16 @@ namespace Reveal.Sdk.Dom.Data
 {
     public class PostgreSqlDataSourceItem : FunctionDataSourceItem, IProcessDataOnServer
     {
+        public PostgreSqlDataSourceItem(string title, string table, PostgreSQLDataSource dataSource) :
+            this(title, dataSource)
+        {
+            Table = table;
+        }
+
+        public PostgreSqlDataSourceItem(string title, PostgreSQLDataSource dataSource) :
+            base(title, dataSource)
+        { }
+
         public PostgreSqlDataSourceItem(string title, DataSource dataSource) :
             base(title, dataSource)
         { }
@@ -14,6 +24,11 @@ namespace Reveal.Sdk.Dom.Data
         {
             get => Properties.GetValue<bool>("ServerAggregation");
             set => Properties.SetItem("ServerAggregation", value);
+        }
+
+        protected override DataSource CreateDataSourceInstance(DataSource dataSource)
+        {
+            return Create<PostgreSQLDataSource>(dataSource);
         }
     }
 }
