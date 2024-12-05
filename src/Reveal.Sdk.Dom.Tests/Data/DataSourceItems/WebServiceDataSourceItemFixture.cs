13c7b1256bf0e3b9f8cbd4dedf60a1c480a769ae
Tan Phat VO <PVo@infragistics.com>
2024-12-05 19:29:51 +0700

    update UTs for webservice and items working with Rdashh file and RDashboard

--- src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/WebServiceDataSourceItemFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/WebServiceDataSourceItemFixture.cs
@@ -1,6 +1,13 @@
+using Newtonsoft.Json.Linq;
 using Reveal.Sdk.Dom.Core.Extensions;
 using Reveal.Sdk.Dom.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
 using Xunit;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Core.Serialization;
 
 namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
 {
@@ -33,6 +40,84 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             // Assert
             Assert.Equal(url, item.Url);
             Assert.Equal(url, item.Properties.GetValue<string>("Url"));
+        }
+
+        [Fact]
+        public void RDashDocument_HasCorrectDataSource_WhenLoadFromFile()
+        {
+            // Arrange
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Dashboards", "TestWebResource.rdash");
+
+            // Act
+            var document = RdashDocument.Load(filePath);
+            var jsonDataSource = document.DataSources.FirstOrDefault();
+            var webServiceDataSource = document.DataSources.LastOrDefault();
+            var webServiceDataSourceItem = document.Visualizations.FirstOrDefault().DataDefinition.DataSourceItem;
+
+            // Assert
+            Assert.Equal(DataSourceProvider.JSON, jsonDataSource.Provider);
+            Assert.Equal(DataSourceProvider.WebService, webServiceDataSource.Provider);
+            Assert.NotNull(webServiceDataSource.Properties.GetValue<string>("Url"));
+            Assert.NotNull(webServiceDataSource.Properties.GetValue<string>("_rpUseAnonymousAuthentication"));
+        }
+
+        [Fact]
+        public void ToJsonString_CreatesFormattedJson_ForWebServiceDataSource()
+        {
+            // Arrange
+            var expectedJson = @"
+            {
+              ""_type"": ""DataSourceItemType"",
+              ""Id"": ""webServiceItemId"",
+              ""Title"": ""Sales by Category"",
+              ""Subtitle"": ""Excel2Json"",
+              ""DataSourceId"": ""webServiceId"",
+              ""HasTabularData"": true,
+              ""HasAsset"": false,
+              ""Properties"": {},
+              ""Parameters"": {}
+            }";
+
+            var dataSource = new WebServiceDataSource()
+            {
+                Id = "webServiceId",
+                Title = "Web Data Source",
+                Subtitle = "Web Data Source Subtitle",
+                Url = "https://excel2json.io/api/share/6e0f06b3-72d3-4fec-7984-08da43f56bb9",
+                UseAnonymousAuthentication = true,
+            };
+
+            var dataSourceItems = new WebServiceDataSourceItem("DB Test", dataSource)
+            {
+                Id = "webServiceItemId",
+                Title = "Sales by Category",
+                Subtitle = "Excel2Json",
+                Fields = new List<IField>
+                {
+                    new NumberField("CategoryID"),
+                    new TextField("CategoryName"),
+                    new TextField("ProductName"),
+                    new NumberField("ProductSales"),
+                }
+            };
+
+            var document = new RdashDocument("My Dashboard");
+            document.Visualizations.Add(
+                new ColumnChartVisualization("Test List", dataSourceItems)
+                    .SetLabel("CategoryName")
+                    .SetValue("ProductName")
+                    );
+
+            var expectedJObject = JObject.Parse(expectedJson);
+
+            // Act
+            RdashSerializer.SerializeObject(document);
+            var json = document.ToJsonString();
+            var jObject = JObject.Parse(json);
+            var actualJObject = jObject["Widgets"].FirstOrDefault()["DataSpec"]["DataSourceItem"];
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
     }
 }
--- src/Reveal.Sdk.Dom.Tests/Data/DataSources/WebServiceDataSourceFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSources/WebServiceDataSourceFixture.cs
@@ -1,5 +1,12 @@
+using Newtonsoft.Json.Linq;
 using Reveal.Sdk.Dom.Core.Extensions;
+using Reveal.Sdk.Dom.Core.Serialization;
 using Reveal.Sdk.Dom.Data;
+using Reveal.Sdk.Dom.Visualizations;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
 using Xunit;
 
 namespace Reveal.Sdk.Dom.Tests.Data.DataSources
@@ -46,6 +53,83 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSources
             // Assert
             Assert.Equal(value, dataSource.UseAnonymousAuthentication);
             Assert.Equal(value, dataSource.Properties.GetValue<bool>("UseAnonymousAuthentication"));
+        }
+
+        [Fact]
+        public void RDashDocument_HasCorrectDataSource_WhenLoadFromFile()
+        {
+            // Arrange
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Dashboards", "TestWebResource.rdash");
+
+            // Act
+            var document = RdashDocument.Load(filePath);
+            var jsonDataSource = document.DataSources.FirstOrDefault();
+            var webServiceDataSource = document.DataSources.LastOrDefault();
+
+            // Assert
+            Assert.Equal(DataSourceProvider.JSON, jsonDataSource.Provider);
+            Assert.Equal(DataSourceProvider.WebService, webServiceDataSource.Provider);
+            Assert.NotNull(webServiceDataSource.Properties.GetValue<string>("Url"));
+            Assert.NotNull(webServiceDataSource.Properties.GetValue<string>("_rpUseAnonymousAuthentication"));
+        }
+
+        [Fact]
+        public void ToJsonString_CreatesFormattedJson_ForWebServiceDataSource()
+        {
+            // Arrange
+            var expectedJson = @"
+            {
+              ""_type"": ""DataSourceType"",
+              ""Id"": ""webServiceId"",
+              ""Provider"": ""WEBSERVICE"",
+              ""Description"": ""Web Data Source"",
+              ""Subtitle"": ""Web Data Source Subtitle"",
+              ""Properties"": {
+                ""URL"": ""https://excel2json.io/api/share/6e0f06b3-72d3-4fec-7984-08da43f56bb9"",
+                ""UseAnonymousAuthentication"": true
+              }
+            }";
+
+            var dataSource = new WebServiceDataSource()
+            {
+                Id = "webServiceId",
+                Title = "Web Data Source",
+                Subtitle = "Web Data Source Subtitle",
+                Url = "https://excel2json.io/api/share/6e0f06b3-72d3-4fec-7984-08da43f56bb9",
+                UseAnonymousAuthentication = true,
+            };
+
+            var dataSourceItems = new WebServiceDataSourceItem("DB Test", dataSource)
+            {
+                Id = "webServiceItemId",
+                Title = "Sales by Category",
+                Subtitle = "Excel2Json",
+                Fields = new List<IField>
+                {
+                    new NumberField("CategoryID"),
+                    new TextField("CategoryName"),
+                    new TextField("ProductName"),
+                    new NumberField("ProductSales"),
+                }
+            };
+
+            var document = new RdashDocument("My Dashboard");
+            document.Visualizations.Add(
+                new ColumnChartVisualization("Test List", dataSourceItems)
+                    .SetLabel("CategoryName")
+                    .SetValue("ProductName")
+                    );
+
+            var expectedJObject = JObject.Parse(expectedJson);
+
+            // Act
+            RdashSerializer.SerializeObject(document);
+            var json = document.ToJsonString();
+            var jObject = JObject.Parse(json);
+            var actualJObject = jObject["DataSources"].FirstOrDefault();
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
     }
 }
