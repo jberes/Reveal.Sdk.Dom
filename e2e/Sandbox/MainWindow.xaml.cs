7aab4b48f444e38d4a8b340af8be3bf18f0be6d6
Tan Phat VO <PVo@infragistics.com>
2024-12-05 19:43:55 +0700

    update sandbox

--- e2e/Sandbox/MainWindow.xaml.cs
+++ e2e/Sandbox/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using Reveal.Sdk.Data.PostgreSQL;
 using Reveal.Sdk.Data.Rest;
 using Reveal.Sdk.Data.Snowflake;
 using Reveal.Sdk.Dom;
+using Sandbox.DashboardCreators;
 using Sandbox.DashboardFactories;
 using Sandbox.RevealSDK;
 using System;
@@ -51,6 +52,7 @@ namespace Sandbox
             new RestDataSourceDashboard(),
             new SalesDashboard(),
             new SqlServerDataSourceDashboards(),
+            new MySqlDataSourceDashboards()
         };
 
         public MainWindow()
@@ -61,6 +63,7 @@ namespace Sandbox
             RevealSdkSettings.AuthenticationProvider = new AuthenticationProvider();
             RevealSdkSettings.DataSources.RegisterMicrosoftSqlServer().RegisterMicrosoftAnalysisServices();
             RevealSdkSettings.DataSources.RegisterMySql();
+
 
             LoadDashboards();
 
