67d3f08fb87268f20ea93b8423b3bf61d0228029
Viktor <VLe@infragistics.com>
2024-12-11 16:22:13 +0700

    Create / update constructors and unit tests for MS Azure Snapse

--- src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/MicrosoftAzureSynapseAnalyticsDataSourceItemFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Data/DataSourceItems/MicrosoftAzureSynapseAnalyticsDataSourceItemFixture.cs
@@ -1,20 +1,50 @@
 ﻿using Reveal.Sdk.Dom.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
 {
     public class MicrosoftAzureSynapseAnalyticsDataSourceItemFixture
     {
+        [Fact]
+        public void Constructor_CreateAzureSynapseDSItem_WithTitle()
+        {
+            // Arrange
+            var expectedDSItemTitle = "Azure Synapse DSI Title";
+
+            // Act
+            var dataSourceItem = new MicrosoftAzureSynapseAnalyticsDataSourceItem(expectedDSItemTitle);
+
+            // Assert
+            Assert.Equal(expectedDSItemTitle, dataSourceItem.Title);
+            Assert.NotNull(dataSourceItem.DataSource);
+            Assert.Equal(expectedDSItemTitle, dataSourceItem.DataSource.Title);
+            Assert.IsType<MicrosoftAzureSynapseAnalyticsDataSource>(dataSourceItem.DataSource);
+        }
 
         [Theory]
         [InlineData("DS Title", "DS Item Title", "DS Title", "DS Item Title")]
         [InlineData(null, "DS Item Title", "DS Item Title", "DS Item Title")]
-        public void Constructor_SetsTitleAndDatasource_AsProvided(string dSTitle, string dSItemTitle, string expectedDSTitle, string expectedDSItemTitle)
+        public void Constructor_CreateDataSource_WithTitleAndDataSource(string dSTitle, string dSItemTitle, string expectedDSTitle, string expectedDSItemTitle)
+        {
+            // Arrange
+            var dataSource = new DataSource() { Title = dSTitle };
+
+            // Act
+            var dataSourceItem = new MicrosoftAzureSynapseAnalyticsDataSourceItem(dSItemTitle, dataSource);
+
+            // Assert
+            Assert.Equal(expectedDSItemTitle, dataSourceItem.Title);
+            Assert.Equal(dataSource, dataSourceItem.DataSource);
+            Assert.Equal(dataSource.Id, dataSourceItem.DataSourceId);
+            Assert.Equal(expectedDSTitle, dataSourceItem.DataSource.Title);
+            Assert.NotSame(dataSource, dataSourceItem.DataSource);
+            Assert.IsType<MicrosoftAzureSynapseAnalyticsDataSource>(dataSourceItem.DataSource);
+        }
+
+        [Theory]
+        [InlineData("DS Title", "DS Item Title", "DS Title", "DS Item Title")]
+        [InlineData(null, "DS Item Title", "DS Item Title", "DS Item Title")]
+        public void Constructor_CreateDataSource_WithTitleAndAzureSynapseDataSource(string dSTitle, string dSItemTitle, string expectedDSTitle, string expectedDSItemTitle)
         {
             // Arrange
             var dataSource = new MicrosoftAzureSynapseAnalyticsDataSource() { Title = dSTitle };
@@ -27,8 +57,7 @@ namespace Reveal.Sdk.Dom.Tests.Data.DataSourceItems
             Assert.Equal(dataSource, dataSourceItem.DataSource);
             Assert.Equal(dataSource.Id, dataSourceItem.DataSourceId);
             Assert.Equal(expectedDSTitle, dataSourceItem.DataSource.Title);
-            Assert.Equal(dataSource, dataSourceItem.DataSource);
-            Assert.Equal(dataSource.Id, dataSourceItem.DataSourceId);
+            Assert.Same(dataSource, dataSourceItem.DataSource);
         }
     }
 }
