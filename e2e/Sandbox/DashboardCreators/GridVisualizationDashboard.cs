eb2fa804ca560f265255889cc6f3d577da6687c9
Tan Phat VO <PVo@infragistics.com>
2024-12-19 21:58:42 +0700

    sandbox tests

---
+++ e2e/Sandbox/DashboardCreators/GridVisualizationDashboard.cs
@@ -0,0 +1,54 @@
+﻿using Reveal.Sdk.Data.Excel;
+using Reveal.Sdk.Dom;
+using Reveal.Sdk.Dom.Data;
+using Reveal.Sdk.Dom.Filters;
+using Reveal.Sdk.Dom.Visualizations;
+using Sandbox.DashboardFactories;
+using Sandbox.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.DashboardCreators
+{
+    internal class GridVisualizationDashboard : IDashboardCreator
+    {
+        public string Name => "Grid Visualization";
+
+        public RdashDocument CreateDashboard()
+        {
+            var document = new RdashDocument("My Dashboard");
+
+            var excelDataSourceItem = new RestDataSourceItem("Marketing Sheet")
+            {
+                Subtitle = "Excel Data Source Item",
+                Url = "http://dl.infragistics.com/reportplus/reveal/samples/Samples.xlsx",
+                IsAnonymous = true,
+                Fields = DataSourceFactory.GetMarketingDataSourceFields(),
+            };
+            excelDataSourceItem.UseExcel("Marketing");
+
+            document.Visualizations.Add(new GridVisualization("Grid", excelDataSourceItem)
+            {
+                IsTitleVisible = true,
+                Description = "Create Grid Visualization"
+            }
+            .SetColumns("Territory", "Conversions", "Spend")
+            .ConfigureSettings(settings =>
+            {
+                settings.IsPagingEnabled = true;
+                settings.IsFirstColumnFixed = true;
+                settings.FontSize = FontSize.Large;
+                settings.DateFieldAlignment = Alignment.Center;
+                settings.TextFieldAlignment = Alignment.Center;
+            }));
+
+            document.Filters.Add(new DashboardDataFilter("Spend", excelDataSourceItem));
+            document.Filters.Add(new DashboardDateFilter("My Date Filter"));
+
+            return document;
+        }
+    }
+}
---
+++ e2e/Sandbox/DashboardCreators/PivotVisualizationDashboard.cs
@@ -0,0 +1,52 @@
+﻿using Reveal.Sdk.Dom;
+using Reveal.Sdk.Dom.Data;
+using Reveal.Sdk.Dom.Filters;
+using Reveal.Sdk.Dom.Visualizations;
+using Sandbox.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.DashboardCreators
+{
+    internal class PivotVisualizationDashboard
+    {
+        public string Name => "Pibot Visualization";
+
+        public RdashDocument CreateDashboard()
+        {
+            var document = new RdashDocument("My Dashboard");
+
+            var excelDataSourceItem = new RestDataSourceItem("Marketing Sheet")
+            {
+                Subtitle = "Excel Data Source Item",
+                Url = "http://dl.infragistics.com/reportplus/reveal/samples/Samples.xlsx",
+                IsAnonymous = true,
+                Fields = DataSourceFactory.GetMarketingDataSourceFields(),
+            };
+            excelDataSourceItem.UseExcel("Marketing");
+
+            document.Visualizations.Add(new PivotVisualization("Pivot", excelDataSourceItem)
+            {
+                IsTitleVisible = true,
+                Description = "Create Grid Visualization"
+            }
+            .SetRow("Territory")
+            .SetValue("New Seats")
+            .SetColumn("CampaignID")
+            .ConfigureSettings(settings =>
+            {
+                settings.FontSize = FontSize.Large;
+                settings.DateFieldAlignment = Alignment.Center;
+                settings.TextFieldAlignment = Alignment.Center;
+                settings.ShowGrandTotals = true;
+            }));
+
+            document.Filters.Add(new DashboardDateFilter("My Date Filter"));
+
+            return document;
+        }
+    }
+}
