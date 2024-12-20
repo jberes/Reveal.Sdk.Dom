87192bd091d54cffe4346d392b489f43a13ee6f2
Viktor <VLe@infragistics.com>
2024-12-20 09:15:06 +0700

    create unit tests for ITabularColumnsExtensions

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Extensions/ITabularColumnsExtensionsFixture.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Visualizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Extensions
+{
+    public class ITabularColumnsExtensionsFixture
+    {
+        [Fact]
+        public void SetColumns_UpdateColumns_WithListFieldNames()
+        {
+            // Arrange
+            var mockTabularColumn = new Mock<ITabularColumns>();
+            mockTabularColumn.SetupGet(s => s.Columns).Returns(new List<TabularColumn> { new TabularColumn("InitialColumn") });
+            var visualization = mockTabularColumn.Object;
+
+            var fieldNames = new List<string> { "FirstField", "SecondField" };
+            var expectedColumns = fieldNames.Select(fieldName => new TabularColumn(fieldName)).ToList();
+
+            // Act
+            visualization.SetColumns(fieldNames.ToArray());
+
+            // Assert
+            Assert.Equivalent(expectedColumns, visualization.Columns);
+        }
+    }
+}
