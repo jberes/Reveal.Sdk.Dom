908334aa3e05f10f7ce82b28f09800fcccfd0ae7
Viktor <VLe@infragistics.com>
2024-12-20 16:57:59 +0700

    Create unit tests for IDateExtensions class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Extensions/IDateExtensionsFixture.cs
@@ -0,0 +1,54 @@
+﻿using Reveal.Sdk.Dom.Visualizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Extensions
+{
+    public class IDateExtensionsFixture
+    {
+        [Fact]
+        public void SetDate_UpdateFieldDate_WithFieldName()
+        {
+            // Arrange
+            var visualization = new MockIDateVisualization()
+            {
+                Date = new DimensionColumn() { DataField = new DateDataField("InitialField") }
+            };
+            var fieldName = "TestField";
+            var expectedDateFieldValue = new DimensionColumn { DataField = new DateDataField(fieldName) };
+
+            // Act
+            visualization.SetDate(fieldName);
+
+            // Assert
+            Assert.Equivalent(expectedDateFieldValue, visualization.Date);
+        }
+
+        [Fact]
+        public void SetDate_UpdateFieldDate_WithDateDataField()
+        {
+            // Arrange
+            var visualization = new MockIDateVisualization()
+            {
+                Date = new DimensionColumn() { DataField = new DateDataField("InitialField") }
+            };
+            var field = new DateDataField("TestField");
+            var expectedDateFieldValue = new DimensionColumn { DataField = field };
+
+            // Act
+            visualization.SetDate(field);
+
+            // Assert
+            Assert.Equivalent(expectedDateFieldValue, visualization.Date);
+        }
+
+        private class MockIDateVisualization : IDate
+        {
+            public DimensionColumn Date { get; set; }
+        }
+    }
+}
