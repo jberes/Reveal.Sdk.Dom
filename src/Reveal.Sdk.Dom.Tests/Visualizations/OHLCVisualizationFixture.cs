23ea6f74501d61474de62c85289f04c4bda0aea5
Phat Vo <recca5p@gmail.com>
2024-12-27 17:37:13 +0700

    update UTs

--- src/Reveal.Sdk.Dom.Tests/Visualizations/OHLCVisualizationFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/OHLCVisualizationFixture.cs
@@ -22,13 +22,31 @@ public class OHLCVisualizationFixture
         Assert.Equal(ChartType.OHLC, ohlcVisualization.ChartType);
         Assert.Null(ohlcVisualization.Title);
         Assert.Null(ohlcVisualization.DataDefinition);
+        Assert.NotNull(ohlcVisualization.Closes);
+        Assert.Empty(ohlcVisualization.Closes);
+        Assert.NotNull(ohlcVisualization.Highs);
+        Assert.Empty(ohlcVisualization.Highs);
+        Assert.NotNull(ohlcVisualization.Lows);
+        Assert.Empty(ohlcVisualization.Lows);
+        Assert.NotNull(ohlcVisualization.Opens);
+        Assert.Empty(ohlcVisualization.Opens);
+        Assert.Equal(0, ohlcVisualization.ColumnSpan);
+        Assert.Equal(0, ohlcVisualization.RowSpan);
+        Assert.Null(ohlcVisualization.Description);
+        Assert.NotNull(ohlcVisualization.Labels);
+        Assert.Empty(ohlcVisualization.Labels);
+        Assert.Null(ohlcVisualization.Linker);
+        Assert.True(ohlcVisualization.IsTitleVisible);
     }
 
-    [Fact]
-    public void Constructor_InitializesOHLCVisualizationWithDataSource_WhenDataSourceItemIsProvided()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Constructor_InitializesOHLCVisualizationWithDataSourceItem_WhenDataSourceItemIsProvided(
+        bool hasTabularData)
     {
         // Arrange
-        var dataSourceItem = new DataSourceItem { HasTabularData = true };
+        var dataSourceItem = new DataSourceItem { HasTabularData = hasTabularData };
 
         // Act
         var ohlcVisualization = new OHLCVisualization(dataSourceItem);
@@ -38,47 +56,74 @@ public class OHLCVisualizationFixture
         Assert.Equal(ChartType.OHLC, ohlcVisualization.ChartType);
         Assert.Equal(dataSourceItem, ohlcVisualization.DataDefinition.DataSourceItem);
         Assert.Null(ohlcVisualization.Title);
+        Assert.Equal(hasTabularData, ohlcVisualization.DataDefinition.DataSourceItem.HasTabularData);
     }
 
+
     [Theory]
-    [InlineData("Stock Analysis", null)]
-    [InlineData(null, null)]
-    public void Constructor_SetsTitleAndDataSource_WhenArgumentsAreProvided(string title, DataSourceItem dataSourceItem)
+    [InlineData("Stock Prices", null, null)]
+    [InlineData(null, null, null)]
+    [InlineData("Stock Prices with Data Source", true, true)]
+    [InlineData("Stock Prices without Tabular Data", false, false)]
+    public void Constructor_SetsTitleAndDataSource_WhenArgumentsAreProvided(string title, bool? hasTabularData,
+        bool? expectedHasTabularData)
     {
+        // Arrange
+        var dataSourceItem = hasTabularData.HasValue
+            ? new DataSourceItem { HasTabularData = hasTabularData.Value }
+            : null;
+
         // Act
         var ohlcVisualization = new OHLCVisualization(title, dataSourceItem);
 
         // Assert
         Assert.Equal(title, ohlcVisualization.Title);
         Assert.Equal(ChartType.OHLC, ohlcVisualization.ChartType);
-        Assert.Null(ohlcVisualization.DataDefinition);
+
+        if (dataSourceItem == null)
+        {
+            Assert.Null(ohlcVisualization.DataDefinition);
+        }
+        else
+        {
+            Assert.NotNull(ohlcVisualization.DataDefinition);
+            Assert.Equal(expectedHasTabularData, ohlcVisualization.DataDefinition.DataSourceItem.HasTabularData);
+        }
     }
 
     [Fact]
-    public void ChartType_HasCorrectValue_WhenInstanceIsCreated()
-    {
-        // Act
-        var ohlcVisualization = new OHLCVisualization();
-
-        // Assert
-        Assert.Equal(ChartType.OHLC, ohlcVisualization.ChartType);
-    }
-
-    [Fact]
-    public void DataDefinition_CanBeUpdated_WhenNewDataSourceItemIsSet()
+    public void UpdateDataSourceItem_UpdatesDataDefinition_WhenNewDataSourceItemIsProvided()
     {
         // Arrange
-        var dataSourceItem = new DataSourceItem { HasTabularData = true };
-        var ohlcVisualization = new OHLCVisualization("Stock Analysis", dataSourceItem);
+        var initialDataSourceItem = new DataSourceItem { HasTabularData = true };
+        var ohlcVisualization = new OHLCVisualization(initialDataSourceItem);
 
-        var newDataSourceItem = new DataSourceItem();
+        var newDataSourceItem = new DataSourceItem { HasTabularData = false };
 
         // Act
         ohlcVisualization.UpdateDataSourceItem(newDataSourceItem);
 
         // Assert
         Assert.Equal(newDataSourceItem, ohlcVisualization.DataDefinition.DataSourceItem);
+        Assert.False(ohlcVisualization.DataDefinition.DataSourceItem.HasTabularData);
+
+        // Ensure other properties remain unaffected
+        Assert.Equal(ChartType.OHLC, ohlcVisualization.ChartType);
+        Assert.Null(ohlcVisualization.Title);
+        Assert.NotNull(ohlcVisualization.DataDefinition);
+        Assert.NotNull(ohlcVisualization.Closes);
+        Assert.Empty(ohlcVisualization.Closes);
+        Assert.NotNull(ohlcVisualization.Highs);
+        Assert.Empty(ohlcVisualization.Highs);
+        Assert.NotNull(ohlcVisualization.Lows);
+        Assert.Empty(ohlcVisualization.Lows);
+        Assert.NotNull(ohlcVisualization.Opens);
+        Assert.Empty(ohlcVisualization.Opens);
+        Assert.True(ohlcVisualization.IsTitleVisible);
+        Assert.Equal(0, ohlcVisualization.ColumnSpan);
+        Assert.Equal(0, ohlcVisualization.RowSpan);
     }
+
 
     [Fact]
     public void ToJsonString_GeneratesCorrectJson_WhenOHLCVisualizationIsSerialized()
@@ -94,51 +139,46 @@ public class OHLCVisualizationFixture
               "ColumnSpan" : 0,
               "RowSpan" : 0,
               "VisualizationSettings" : {
-                "_type" : "PivotVisualizationSettingsType",
-                "FontSize" : "Large",
-                "Style" : {
-                  "FixedLeftColumns" : false,
-                  "TextAlignment" : "Center",
-                  "NumericAlignment" : "Inherit",
-                  "DateAlignment" : "Center"
-                },
-                "VisualizationType" : "PIVOT"
+                "_type" : "SampleSchema",
+                "LeftAxisLogarithmic" : false,
+                "ChartType" : "OHLC",
+                "VisualizationType" : "CHART"
               },
               "DataSpec" : {
                 "_type" : "TabularDataSpecType",
                 "IsTransposed" : false,
                 "Fields" : [ {
-                  "FieldName" : "Date",
-                  "FieldLabel" : "Date",
-                  "UserCaption" : "Date",
-                  "IsCalculated" : false,
-                  "Properties" : { },
-                  "Sorting" : "None",
-                  "FieldType" : "Date"
-                }, {
-                  "FieldName" : "Spend",
-                  "FieldLabel" : "Spend",
-                  "UserCaption" : "Spend",
+                  "FieldName" : "Open",
+                  "FieldLabel" : "Open",
+                  "UserCaption" : "Open",
                   "IsCalculated" : false,
                   "Properties" : { },
                   "Sorting" : "None",
                   "FieldType" : "Number"
                 }, {
-                  "FieldName" : "Conversions",
-                  "FieldLabel" : "Conversions",
-                  "UserCaption" : "Conversions",
+                  "FieldName" : "High",
+                  "FieldLabel" : "High",
+                  "UserCaption" : "High",
                   "IsCalculated" : false,
                   "Properties" : { },
                   "Sorting" : "None",
                   "FieldType" : "Number"
                 }, {
-                  "FieldName" : "Territory",
-                  "FieldLabel" : "Territory",
-                  "UserCaption" : "Territory",
+                  "FieldName" : "Low",
+                  "FieldLabel" : "Low",
+                  "UserCaption" : "Low",
                   "IsCalculated" : false,
                   "Properties" : { },
                   "Sorting" : "None",
-                  "FieldType" : "String"
+                  "FieldType" : "Number"
+                }, {
+                  "FieldName" : "Close",
+                  "FieldLabel" : "Close",
+                  "UserCaption" : "Close",
+                  "IsCalculated" : false,
+                  "Properties" : { },
+                  "Sorting" : "None",
+                  "FieldType" : "Number"
                 } ],
                 "TransposedFields" : [ ],
                 "QuickFilters" : [ ],
@@ -176,37 +216,111 @@ public class OHLCVisualizationFixture
                 }
               },
               "VisualizationDataSpec" : {
-                "_type" : "PivotVisualizationDataSpecType",
-                "Columns" : [ {
-                  "_type" : "DimensionColumnSpecType",
+                "_type" : "FinancialVisualizationDataSpecType",
+                "Open" : [ {
+                  "_type" : "MeasureColumnSpecType",
                   "SummarizationField" : {
-                    "_type" : "SummarizationRegularFieldType",
-                    "DrillDownElements" : [ ],
-                    "ExpandedItems" : [ ],
-                    "FieldName" : "Territory"
-                  }
-                }, {
-                  "_type" : "DimensionColumnSpecType",
-                  "SummarizationField" : {
-                    "_type" : "SummarizationRegularFieldType",
-                    "DrillDownElements" : [ ],
-                    "ExpandedItems" : [ ],
-                    "FieldName" : "Conversions"
-                  }
-                }, {
-                  "_type" : "DimensionColumnSpecType",
-                  "SummarizationField" : {
-                    "_type" : "SummarizationRegularFieldType",
-                    "DrillDownElements" : [ ],
-                    "ExpandedItems" : [ ],
-                    "FieldName" : "Spend"
+                    "_type" : "SummarizationValueFieldType",
+                    "FieldLabel" : "Open",
+                    "UserCaption" : "Open",
+                    "IsHidden" : false,
+                    "AggregationType" : "Sum",
+                    "Sorting" : "None",
+                    "IsCalculated" : false,
+                    "Formatting" : {
+                      "_type" : "NumberFormattingSpecType",
+                      "ApplyMkFormat" : false,
+                      "CurrencySymbol" : "$",
+                      "DecimalDigits" : 0,
+                      "FormatType" : "Number",
+                      "NegativeFormat" : "MinusSign",
+                      "ShowGroupingSeparator" : true,
+                      "OverrideDefaultSettings" : false
+                    },
+                    "FieldName" : "Open"
                   }
                 } ],
-                "Values" : [ ],
-                "ShowGrandTotals" : false,
+                "High" : [ {
+                  "_type" : "MeasureColumnSpecType",
+                  "SummarizationField" : {
+                    "_type" : "SummarizationValueFieldType",
+                    "FieldLabel" : "High",
+                    "UserCaption" : "High",
+                    "IsHidden" : false,
+                    "AggregationType" : "Sum",
+                    "Sorting" : "None",
+                    "IsCalculated" : false,
+                    "Formatting" : {
+                      "_type" : "NumberFormattingSpecType",
+                      "ApplyMkFormat" : false,
+                      "CurrencySymbol" : "$",
+                      "DecimalDigits" : 0,
+                      "FormatType" : "Number",
+                      "NegativeFormat" : "MinusSign",
+                      "ShowGroupingSeparator" : true,
+                      "OverrideDefaultSettings" : false
+                    },
+                    "FieldName" : "High"
+                  }
+                } ],
+                "Low" : [ {
+                  "_type" : "MeasureColumnSpecType",
+                  "SummarizationField" : {
+                    "_type" : "SummarizationValueFieldType",
+                    "FieldLabel" : "Low",
+                    "UserCaption" : "Low",
+                    "IsHidden" : false,
+                    "AggregationType" : "Sum",
+                    "Sorting" : "None",
+                    "IsCalculated" : false,
+                    "Formatting" : {
+                      "_type" : "NumberFormattingSpecType",
+                      "ApplyMkFormat" : false,
+                      "CurrencySymbol" : "$",
+                      "DecimalDigits" : 0,
+                      "FormatType" : "Number",
+                      "NegativeFormat" : "MinusSign",
+                      "ShowGroupingSeparator" : true,
+                      "OverrideDefaultSettings" : false
+                    },
+                    "FieldName" : "Low"
+                  }
+                } ],
+                "Close" : [ {
+                  "_type" : "MeasureColumnSpecType",
+                  "SummarizationField" : {
+                    "_type" : "SummarizationValueFieldType",
+                    "FieldLabel" : "Close",
+                    "UserCaption" : "Close",
+                    "IsHidden" : false,
+                    "AggregationType" : "Sum",
+                    "Sorting" : "None",
+                    "IsCalculated" : false,
+                    "Formatting" : {
+                      "_type" : "NumberFormattingSpecType",
+                      "ApplyMkFormat" : false,
+                      "CurrencySymbol" : "$",
+                      "DecimalDigits" : 0,
+                      "FormatType" : "Number",
+                      "NegativeFormat" : "MinusSign",
+                      "ShowGroupingSeparator" : true,
+                      "OverrideDefaultSettings" : false
+                    },
+                    "FieldName" : "Close"
+                  }
+                } ],
                 "FormatVersion" : 0,
                 "AdHocExpandedElements" : [ ],
-                "Rows" : [ ]
+                "Rows" : [ {
+                  "_type" : "DimensionColumnSpecType",
+                  "SummarizationField" : {
+                    "_type" : "SummarizationDateFieldType",
+                    "DateAggregationType" : "Day",
+                    "DrillDownElements" : [ ],
+                    "ExpandedItems" : [ ],
+                    "FieldName" : "Date"
+                  }
+                } ]
               }
             } ]
             """;
@@ -235,32 +349,33 @@ public class OHLCVisualizationFixture
             },
             Fields = new List<IField>
             {
-                new DateField("Date"),
-                new NumberField("Spend"),
-                new NumberField("Conversions"),
-                new TextField("Territory")
+                new NumberField("Open"),
+                new NumberField("High"),
+                new NumberField("Low"),
+                new NumberField("Close")
             }
         };
         excelDataSourceItem.UseExcel("Marketing");
 
-        document.Visualizations.Add(new PivotVisualization("OHLC Visualization", excelDataSourceItem)
+        document.Visualizations.Add(new OHLCVisualization("OHLC Visualization", excelDataSourceItem)
             {
                 Id = "60344a4a-d0ce-4364-9f8c-acfbb8caa32e",
                 IsTitleVisible = true,
                 Description = "Create OHLC Visualization"
             }
-            .SetColumns("Territory", "Conversions", "Spend")
+            .SetLabel(new DateDataField("Date") { AggregationType = DateAggregationType.Day })
+            .SetOpen("Open")
+            .SetHigh("High")
+            .SetLow("Low")
+            .SetClose("Close")
             .ConfigureSettings(settings =>
             {
-                settings.FontSize = FontSize.Large;
-                settings.DateFieldAlignment = Alignment.Center;
-                settings.TextFieldAlignment = Alignment.Center;
+                settings.SchemaTypeName = "SampleSchema";
             }));
 
         document.Filters.Add(new DashboardDataFilter("Spend", excelDataSourceItem));
         document.Filters.Add(new DashboardDateFilter("My Date Filter"));
 
-        //Act
         RdashSerializer.SerializeObject(document);
         var json = document.ToJsonString();
         var actualJson = JObject.Parse(json)["Widgets"];
