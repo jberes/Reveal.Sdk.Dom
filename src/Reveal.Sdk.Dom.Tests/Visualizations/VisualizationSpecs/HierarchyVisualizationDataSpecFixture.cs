b65a70d20442eefb4d9a85622a8deade20a7ee5f
Viktor <VLe@infragistics.com>
2024-12-31 16:47:38 +0700

    Create unit test for HierarchyVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/HierarchyVisualizationDataSpecFixture.cs
@@ -0,0 +1,20 @@
+﻿using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class HierarchyVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var hierarchyVSDataSpec = new HierarchyVisualizationDataSpec();
+
+            // Assert
+            Assert.Empty(hierarchyVSDataSpec.AdHocExpandedElements);
+            Assert.Empty(hierarchyVSDataSpec.Rows);
+            Assert.Null(hierarchyVSDataSpec.AdHocFields);
+        }
+    }
+}
