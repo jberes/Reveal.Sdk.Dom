1cc658bceae87aa919095610b5196a164cbbcb51
Viktor <VLe@infragistics.com>
2024-12-31 16:55:51 +0700

    Create unit test for ScatterMapVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/ScatterMapVisualizationDataSpecFixture.cs
@@ -0,0 +1,22 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class ScatterMapVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_CreateObjWithDefaultValue_WithoutParameters()
+        {
+            // Act
+            var scatterMapVSDataSpec = new ScatterMapVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.ScatterMapVisualizationDataSpecType, scatterMapVSDataSpec.SchemaTypeName);
+            Assert.True(scatterMapVSDataSpec.IsColorByValue);
+            Assert.Empty(scatterMapVSDataSpec.MapColor);
+            Assert.Empty(scatterMapVSDataSpec.Radius);
+        }
+    }
+}
