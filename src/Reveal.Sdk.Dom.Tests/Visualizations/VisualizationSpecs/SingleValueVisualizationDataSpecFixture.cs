d132d2051d7db1550a522a637ad91352dc939e05
Viktor <VLe@infragistics.com>
2024-12-31 17:04:40 +0700

    Craete unit test for SingleValueVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/SingleValueVisualizationDataSpecFixture.cs
@@ -0,0 +1,20 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class SingleValueVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsObjHaveDefaultValue_WithoutParmeter()
+        {
+            // Act
+            var singleValueVSDataSpec = new SingleValueVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.SingleValueVisualizationDataSpecType, singleValueVSDataSpec.SchemaTypeName);
+            Assert.Empty(singleValueVSDataSpec.Value);
+        }
+    }
+}
