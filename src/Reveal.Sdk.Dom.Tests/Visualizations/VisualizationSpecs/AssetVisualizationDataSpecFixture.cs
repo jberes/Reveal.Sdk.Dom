d2854427a6203af0c7d1f35c0a17ae24a7643507
Viktor <VLe@infragistics.com>
2024-12-31 16:26:19 +0700

    Create Unit tests for AssetVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/AssetVisualizationDataSpecFixture.cs
@@ -0,0 +1,20 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class AssetVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_CreateObjWithDefaultValue_WithoutParameters()
+        {
+            // Act
+            var assertVSDataSpec = new AssetVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.AssetVisualizationDataSpecType, assertVSDataSpec.SchemaTypeName);
+            Assert.Null(assertVSDataSpec.UrlColumn);
+        }
+    }
+}
