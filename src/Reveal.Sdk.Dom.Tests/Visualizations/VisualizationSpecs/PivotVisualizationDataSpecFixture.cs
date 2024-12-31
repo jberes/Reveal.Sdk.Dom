db8c7a5aed73e40ac78fa0902b926726343aa10a
Viktor <VLe@infragistics.com>
2024-12-31 17:09:03 +0700

    Create unit test for PivotVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/PivotVisualizationDataSpecFixture.cs
@@ -0,0 +1,21 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class PivotVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var pivotVSDataSpec = new PivotVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.PivotVisualizationDataSpecType, pivotVSDataSpec.SchemaTypeName);
+            Assert.Empty(pivotVSDataSpec.Columns);
+            Assert.Empty(pivotVSDataSpec.Values);
+        }
+    }
+}
