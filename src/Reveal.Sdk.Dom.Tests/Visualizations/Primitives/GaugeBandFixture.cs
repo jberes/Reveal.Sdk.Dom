7f0ccb02b044a859c4cd842ff917077261482d1c
Viktor <VLe@infragistics.com>
2025-01-02 19:54:55 +0700

    Create UTs for GaugeBand class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/GaugeBandFixture.cs
@@ -0,0 +1,19 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class GaugeBandFixture
+    {
+        [Fact]
+        public void Constructor_SetDefaultSchemaTypeName_WithoutParameters()
+        {
+            // Act
+            var gaugeBand = new GaugeBand();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.GaugeBandType, gaugeBand.SchemaTypeName);
+        }
+    }
+}
