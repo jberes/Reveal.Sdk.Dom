56fa4ced41fc190718afb450c46586caf8b3ee11
Henry <hnguyen@infragistics.com>
2025-01-02 21:41:54 +0700

    fix test dimension column

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/DimensionColumnFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/DimensionColumnFixture.cs
@@ -1,12 +1,8 @@
 using Newtonsoft.Json.Linq;
 using Reveal.Sdk.Dom.Visualizations;
 using Reveal.Sdk.Dom.Core.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
+using Newtonsoft.Json;
 
 namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
 {
@@ -35,5 +31,51 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
             Assert.Equal(dataField, column.DataField);
         }
 
+        [Fact]
+        public void ConvertToJson_UseExpectedFields_WhenCalled()
+        {
+            // Arrange
+            var dataField = new TextDataField("test field");
+            var column = new DimensionColumn(dataField);
+            column.DataField = new TextDataField("test field");
+            column.XmlaElement = new TestXmlaDimensionElement();
+
+            var expectedJson = JObject.Parse("""
+            {
+              "_type": "DimensionColumnSpecType",
+              "SummarizationField": {
+                "_type": "SummarizationRegularFieldType",
+                "DrillDownElements": [],
+                "ExpandedItems": [],
+                "FieldName": "test field"
+              },
+              "XmlaElement": {
+                "DimensionType": "Regular",
+                "DrillDownElements": [],
+                "Sorting": "None",
+                "FieldSortingByLabel": false,
+                "FullyExpandedLevels": 0,
+                "ExpandedItems": [],
+                "DateAggregationType": "Year",
+                "DateFiscalYearStartMonth": 0,
+                "DrillDownMembers": []
+              }
+            }
+            """);
+
+            // Act
+            var outputJson = JsonConvert.SerializeObject(column, Formatting.Indented, new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            // Assert
+            Assert.Equal(expectedJson.ToString(), outputJson);
+        }
+
+        private class TestXmlaDimensionElement : XmlaDimensionElement
+        {
+        }
     }
 }
