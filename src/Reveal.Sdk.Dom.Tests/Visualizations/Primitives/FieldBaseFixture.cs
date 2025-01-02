c55a3807ed965fd7b5e5d9cbd319db832a6c242a
Viktor <VLe@infragistics.com>
2025-01-02 19:45:04 +0700

    Add unit test for ToJsonString method

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/FieldBaseFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/FieldBaseFixture.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
+using Newtonsoft.Json.Linq;
 using Reveal.Sdk.Dom.Filters;
 using Reveal.Sdk.Dom.Visualizations;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
@@ -31,6 +33,51 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
             // Assert
             Assert.Equal(testFieldName, fieldBase.FieldName);
             Assert.Equal(testFieldName, fieldBase.FieldLabel);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJson_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "FieldName": "TestFieldName",
+                  "FieldLabel": "TestFieldLabel",
+                  "UserCaption": "TestUserCaption",
+                  "IsCalculated": false,
+                  "Expression": "Test Expression",
+                  "Filter": {},
+                  "Properties": {
+                    "TestKey": "TestValue"
+                  },
+                  "Sorting": "Asc",
+                  "TableAlias": "TestTableAlias",
+                  "FieldType": "String"
+                }
+                """;
+            var fieldBase = new MockFieldBase("TestFieldName")
+            {
+                DataFilter = new MockIFilter(),
+                Expression = "Test Expression",
+                FieldLabel = "TestFieldLabel",
+                FieldName = "TestFieldName",
+                IsCalculated = false,
+                Properties = new Dictionary<string, object>
+                {
+                    { "TestKey", "TestValue" }
+                },
+                Sorting = SortingType.Asc,
+                TableAlias = "TestTableAlias",
+                UserCaption = "TestUserCaption"
+            };
+
+            // Act
+            var actualJson = fieldBase.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
 
         private class MockFieldBase : FieldBase<MockIFilter>
