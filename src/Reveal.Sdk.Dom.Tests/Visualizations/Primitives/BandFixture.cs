fe60a9583a659efa0459cc6ae4500323608e57b5
Viktor <VLe@infragistics.com>
2025-01-02 15:58:50 +0700

    Create UTs for Band class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/BandFixture.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class BandFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Arrange
+            var mockBand = new Mock<Band>();
+
+            // Act
+            var band = mockBand.Object;
+
+            // Assert
+            Assert.Equal(ValueComparisonType.Percentage, band.ValueComparisonType);
+            Assert.Equal(BandColor.Green, band.Color);
+        }
+    }
+}
