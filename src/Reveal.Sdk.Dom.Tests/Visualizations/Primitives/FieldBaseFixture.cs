d2635f65da12c00c5edf36af0933551fad739f4d
Viktor <VLe@infragistics.com>
2025-01-02 19:23:26 +0700

    Create UT for FieldBase

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/FieldBaseFixture.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using Reveal.Sdk.Dom.Filters;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class FieldBaseFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithFieldNameParameter()
+        {
+            // Act
+            var fieldBase = new MockFieldBase("TestFieldName");
+
+            // Assert
+            Assert.Equal(DataType.String, ((IFieldDataType)fieldBase).DataType);
+            Assert.Empty(fieldBase.Properties);
+            Assert.Equal(SortingType.None, fieldBase.Sorting);
+        }
+
+        [Fact]
+        public void Constructor_UpdateFieldNameAndFieldLabel_WithFieldNameParameter()
+        {
+            // Arrange
+            var testFieldName = "Field Base Field Name";
+
+            // Act
+            var fieldBase = new MockFieldBase(testFieldName);
+
+            // Assert
+            Assert.Equal(testFieldName, fieldBase.FieldName);
+            Assert.Equal(testFieldName, fieldBase.FieldLabel);
+        }
+
+        private class MockFieldBase : FieldBase<MockIFilter>
+        {
+            public MockFieldBase(string fieldName) : base(fieldName)
+            {
+            }
+        }
+
+        private class MockIFilter : IFilter
+        { }
+
+        private class MockField : FieldBase<MockIFilter>
+        {
+            public MockField(string fieldName) : base(fieldName)
+            { }
+        }
+    }
+}
