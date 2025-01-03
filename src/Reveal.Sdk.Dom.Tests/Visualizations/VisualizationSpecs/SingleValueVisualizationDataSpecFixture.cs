55c2b08706b896697cfedcbe3fe1eb66f504db82
Viktor <VLe@infragistics.com>
2025-01-03 11:46:54 +0700

    Add ToJsonString unit test

--- src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/SingleValueVisualizationDataSpecFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/SingleValueVisualizationDataSpecFixture.cs
@@ -1,5 +1,7 @@
-﻿using Reveal.Sdk.Dom.Core.Constants;
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
 using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
@@ -15,6 +17,31 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
             // Assert
             Assert.Equal(SchemaTypeNames.SingleValueVisualizationDataSpecType, singleValueVSDataSpec.SchemaTypeName);
             Assert.Empty(singleValueVSDataSpec.Value);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "Test Schema Type Name",
+                  "Value": []
+                }
+                """;
+            var singleValueVSDataSpec = new SingleValueVisualizationDataSpec()
+            {
+                SchemaTypeName = "Test Schema Type Name",
+                Value = new List<Dom.Visualizations.MeasureColumn>()
+            };
+
+            // Act
+            var actualJson = singleValueVSDataSpec.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
     }
 }
