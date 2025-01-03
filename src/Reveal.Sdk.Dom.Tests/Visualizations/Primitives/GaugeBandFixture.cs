98e50ede43f180b0d445d8b054c5685027783f6f
Viktor <VLe@infragistics.com>
2025-01-03 11:57:07 +0700

    Create UT for ToJsonString

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/GaugeBandFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/GaugeBandFixture.cs
@@ -1,4 +1,5 @@
-﻿using Reveal.Sdk.Dom.Core.Constants;
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
 using Reveal.Sdk.Dom.Visualizations;
 using Xunit;
 
@@ -14,6 +15,35 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
 
             // Assert
             Assert.Equal(SchemaTypeNames.GaugeBandType, gaugeBand.SchemaTypeName);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJson_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "Test Schema Type Name",
+                  "Type": "NumberValue",
+                  "Color": "Red",
+                  "Value": 12.5
+                }
+                """;
+            var gaugeBand = new GaugeBand()
+            {
+                Color = BandColor.Red,
+                SchemaTypeName = "Test Schema Type Name",
+                Value = 12.5,
+                ValueComparisonType = ValueComparisonType.Number
+            };
+
+            // Act
+            var actualJson = gaugeBand.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
     }
 }
