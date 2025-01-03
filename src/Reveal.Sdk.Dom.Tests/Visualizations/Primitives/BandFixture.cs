547a874c88ea07c9ecda82d7de1d37b2102d272a
Viktor <VLe@infragistics.com>
2025-01-03 11:32:38 +0700

    Update UTs for Band class

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/BandFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/BandFixture.cs
@@ -1,4 +1,4 @@
-﻿using Moq;
+﻿using Newtonsoft.Json.Linq;
 using Reveal.Sdk.Dom.Visualizations;
 using Xunit;
 
@@ -9,15 +9,44 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
         [Fact]
         public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
         {
-            // Arrange
-            var mockBand = new Mock<Band>();
-
             // Act
-            var band = mockBand.Object;
+            var band = new MockBand();
 
             // Assert
             Assert.Equal(ValueComparisonType.Percentage, band.ValueComparisonType);
             Assert.Equal(BandColor.Green, band.Color);
+            Assert.Null(band.Value);
         }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "TestSchemaTypeName",
+                  "Type": "NumberValue",
+                  "Color": "Green",
+                  "Value": 12.5
+                }
+                """;
+            var band = new MockBand()
+            {
+                Color = BandColor.Green,
+                SchemaTypeName = "TestSchemaTypeName",
+                Value = 12.5,
+                ValueComparisonType = ValueComparisonType.Number
+            };
+
+            // Act
+            var actualJson = band.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
+        }
+
+        private class MockBand : Band { }
     }
 }
