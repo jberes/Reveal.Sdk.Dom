74ab98a095e3267ca93052657a4f59f5c7e90706
Henry <hnguyen@infragistics.com>
2025-01-03 14:28:57 +0700

    add test for map conditional formating band

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/MapConditionalFormattingBandFixture.cs
@@ -0,0 +1,19 @@
+﻿using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Core.Constants;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class MapConditionalFormattingBandFixture
+    {
+        [Fact]
+        public void Constructor_SetSchemaTypeNames_WhenConstructed()
+        {
+            // Act
+            var item = new MapConditionalFormattingBand();
+
+            // Arrange
+            Assert.Equal(SchemaTypeNames.ConditionalFormattingBandType, item.SchemaTypeName);
+        }
+    }
+}
