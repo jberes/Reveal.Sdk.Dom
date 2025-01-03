21618ef410ea86d9e3ad408835cfe27d5393c61b
Henry <hnguyen@infragistics.com>
2025-01-03 12:20:05 +0700

    Add Conditional formatting tests

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/ConditionalFormattingBandFixture.cs
@@ -0,0 +1,25 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Data;
+using Reveal.Sdk.Dom.Visualizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class ConditionalFormattingBandFixture
+    {
+        [Fact]
+        public void Constructor_SetDefaultValues_WhenConstructed()
+        {
+            // Act
+            var item = new ConditionalFormattingBand();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.ConditionalFormattingBandType, item.SchemaTypeName);
+        }
+    }
+}
