43f6279ca3de53a959d5fdbaac9c8e1c701be89f
Henry <hnguyen@infragistics.com>
2025-01-03 11:37:52 +0700

    Add Conditional Formatting Test

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/ConditionalFormattingFixture.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Reveal.Sdk.Dom.Visualizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class ConditionalFormattingFixture
+    {
+        [Fact]
+        public void Constructor_SetDefaultBounds_WhenCreated()
+        {
+            // Arrange
+            var expectedMinimum = new Bound() { ValueType = BoundValueType.LowestValue };
+            var expectedMaximum = new Bound() { ValueType = BoundValueType.HighestValue };
+
+            // Act
+            var conditionalFormatting = new ConditionalFormatting();
+
+            // Assert
+            Assert.Equivalent(expectedMinimum, conditionalFormatting.Minimum);
+            Assert.Equivalent(expectedMaximum, conditionalFormatting.Maximum);
+        }
+    }
+}
