56da546678814513a3bd5d5b5850f5804d638834
Henry <hnguyen@infragistics.com>
2025-01-06 15:31:11 +0700

    add UTs for TextDataField primitives

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/TextDataFieldFixture.cs
@@ -0,0 +1,34 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class TextDataFieldFixture
+    {
+        [Fact]
+        public void Constructor_SetsDefaultValues_WhenConstructedWithNoFieldName()
+        {
+            // Act
+            var band = new TextDataField();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.SummarizationRegularFieldType, band.SchemaTypeName);
+            Assert.Equal(string.Empty, band.FieldName);
+        }
+
+        [Fact]
+        public void Constructor_SetsDefaultValues_WhenConstructedWithFieldName()
+        {
+            // Arrange
+            var fieldName = "TestFieldName";
+
+            // Act
+            var band = new TextDataField(fieldName);
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.SummarizationRegularFieldType, band.SchemaTypeName);
+            Assert.Equal(fieldName, band.FieldName);
+        }
+    }
+}
