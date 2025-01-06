75274171c2304fcb03e680910db0ee5a4e2ce598
Viktor <VLe@infragistics.com>
2025-01-06 15:36:38 +0700

    Create unit test for IndicatorVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/IndicatorVisualizationDataSpecFixture.cs
@@ -0,0 +1,21 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class IndicatorVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var indicatorVSDataSpec = new IndicatorVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(IndicatorVisualizationType.YearToDatePreviousYear, indicatorVSDataSpec.IndicatorType);
+            Assert.Equal(SchemaTypeNames.IndicatorVisualizationDataSpecType, indicatorVSDataSpec.SchemaTypeName);
+        }
+    }
+}
