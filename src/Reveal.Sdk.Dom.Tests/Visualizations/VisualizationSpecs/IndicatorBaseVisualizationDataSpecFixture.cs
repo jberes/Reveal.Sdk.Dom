1aba82b806c00fbc5e50ad6f6cef1e562a1c3755
Viktor <VLe@infragistics.com>
2025-01-06 14:39:16 +0700

    Create UTs for IndicatorBaseVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/IndicatorBaseVisualizationDataSpecFixture.cs
@@ -0,0 +1,20 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class IndicatorBaseVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var indicator = new IndicatorBaseVisualizationDataSpec();
+
+            // Assert
+            Assert.Empty(indicator.Value);
+            Assert.Equal(SchemaTypeNames.IndicatorBaseVisualizationDataSpecType, indicator.SchemaTypeName);
+        }
+    }
+}
