9eec39ce81d370f3628e3122ff833d2cf18a14e7
Henry <hnguyen@infragistics.com>
2025-01-06 15:16:26 +0700

    add UTs for TextBand primitive

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/TextBandFixture.cs
@@ -0,0 +1,19 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class TextBandFixture
+    {
+        [Fact]
+        public void Constructor_SetsSchemaTypeName_WhenConstructed()
+        {
+            // Act
+            var band = new TextBand();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.GaugeBandType, band.SchemaTypeName);
+        }
+    }
+}
