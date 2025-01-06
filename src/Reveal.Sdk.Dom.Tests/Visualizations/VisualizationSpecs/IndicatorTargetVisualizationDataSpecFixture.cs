3fcf9c29f2909bb5f63634ebd5b45de5b1275b98
Viktor <VLe@infragistics.com>
2025-01-06 14:50:24 +0700

    Create UTs for IndicatorTargetVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/IndicatorTargetVisualizationDataSpecFixture.cs
@@ -0,0 +1,22 @@
+﻿using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class IndicatorTargetVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var indicatorTargetVSDataSpec = new IndicatorTargetVisualizationDataSpec();
+
+            // Assert
+            Assert.Empty(indicatorTargetVSDataSpec.Target);
+            Assert.Equal(KpiGoalPeriod.YearToDate, indicatorTargetVSDataSpec.DateFilterType);
+            Assert.Equal(SchemaTypeNames.IndicatorTargetVisualizationDataSpecType, indicatorTargetVSDataSpec.SchemaTypeName);
+        }
+    }
+}
