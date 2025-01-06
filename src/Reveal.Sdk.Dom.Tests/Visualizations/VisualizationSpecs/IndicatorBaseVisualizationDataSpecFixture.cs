a30ac308c0a346cb08fcb7d24f5ceb23a3122978
Viktor <VLe@infragistics.com>
2025-01-06 16:09:33 +0700

    Add unit test for ToJsonString method

--- src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/IndicatorBaseVisualizationDataSpecFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/IndicatorBaseVisualizationDataSpecFixture.cs
@@ -1,5 +1,7 @@
-﻿using Reveal.Sdk.Dom.Core.Constants;
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
 using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
@@ -15,6 +17,43 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
             // Assert
             Assert.Empty(indicator.Value);
             Assert.Equal(SchemaTypeNames.IndicatorBaseVisualizationDataSpecType, indicator.SchemaTypeName);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "AssetVisualizationSettingsType",
+                  "Date": {
+                    "_type": "DimensionColumnSpecType"
+                  },
+                  "Value": [],
+                  "AdHocFields": 11,
+                  "FormatVersion": 3,
+                  "AdHocExpandedElements": [],
+                  "Rows": []
+                }
+                """;
+            var baseIndicator = new IndicatorBaseVisualizationDataSpec()
+            {
+                AdHocExpandedElements = new List<Dom.Visualizations.AdHocExpandedElement>(),
+                AdHocFields = 11,
+                Date = new Dom.Visualizations.DimensionColumn(),
+                FormatVersion = 3,
+                Rows = new List<Dom.Visualizations.DimensionColumn>(),
+                SchemaTypeName = SchemaTypeNames.AssetVisualizationSettingsType,
+                Value = new List<Dom.Visualizations.MeasureColumn>()
+            };
+
+            // Act
+            var actualJson = baseIndicator.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
         }
     }
 }
