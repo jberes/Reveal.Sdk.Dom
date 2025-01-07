e31d6a0db0578048d2fe9007511e23d3b6aa52d2
Viktor <VLe@infragistics.com>
2025-01-07 20:26:29 +0700

    Create UTs for BubbleVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/BubbleVisualizationDataSpecFixture.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class BubbleVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue()
+        {
+            // Act
+            var bubbleVSDataSpec = new BubbleVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.BubbleVisualizationDataSpecType, bubbleVSDataSpec.SchemaTypeName);
+            Assert.Empty(bubbleVSDataSpec.Radius);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "ChartVisualizationSettingsType",
+                  "Radius": [],
+                  "Category": {
+                    "_type": "DimensionColumnSpecType"
+                  },
+                  "XAxis": [],
+                  "YAxis": [],
+                  "AdHocFields": 13,
+                  "FormatVersion": 5,
+                  "AdHocExpandedElements": [],
+                  "Rows": []
+                }
+                """;
+            var bubbleVSDataSpec = new BubbleVisualizationDataSpec()
+            {
+                AdHocExpandedElements = new List<Dom.Visualizations.AdHocExpandedElement>(),
+                AdHocFields = 13,
+                FormatVersion = 5,
+                Rows = new List<Dom.Visualizations.DimensionColumn>(),
+                SchemaTypeName = SchemaTypeNames.ChartVisualizationSettingsType,
+                Category = new Dom.Visualizations.DimensionColumn(),
+                Radius = new List<Dom.Visualizations.MeasureColumn>(),
+                XAxis = new List<Dom.Visualizations.MeasureColumn>(),
+                YAxis = new List<Dom.Visualizations.MeasureColumn>()
+            };
+
+            // Act
+            var actualJson = bubbleVSDataSpec.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
+        }
+    }
+}
