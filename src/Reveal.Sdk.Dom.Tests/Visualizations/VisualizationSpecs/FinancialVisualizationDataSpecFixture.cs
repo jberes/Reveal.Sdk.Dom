f2138c5da61e9374c3e5ae29cdf12e1b50502a0a
Viktor <VLe@infragistics.com>
2025-01-08 15:24:52 +0700

    Create UTs for FinancialVisualizationDataSpec class

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/FinancialVisualizationDataSpecFixture.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
+{
+    public class FinancialVisualizationDataSpecFixture
+    {
+        [Fact]
+        public void Constructor_FieldsHaveDefaultValue_WithoutParameters()
+        {
+            // Act
+            var financialVSDataSpec = new FinancialVisualizationDataSpec();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.FinancialVisualizationDataSpecType, financialVSDataSpec.SchemaTypeName);
+            Assert.Empty(financialVSDataSpec.Open);
+            Assert.Empty(financialVSDataSpec.Close);
+            Assert.Empty(financialVSDataSpec.High);
+            Assert.Empty(financialVSDataSpec.Low);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var expectedJson = """
+                {
+                  "_type": "ChartVisualizationSettingsType",
+                  "Open": [],
+                  "High": [],
+                  "Low": [],
+                  "Close": [],
+                  "AdHocFields": 13,
+                  "FormatVersion": 5,
+                  "AdHocExpandedElements": [],
+                  "Rows": []
+                }
+                """;
+            var financialVSDataSpec = new FinancialVisualizationDataSpec()
+            {
+                AdHocExpandedElements = new List<Dom.Visualizations.AdHocExpandedElement>(),
+                AdHocFields = 13,
+                FormatVersion = 5,
+                Rows = new List<Dom.Visualizations.DimensionColumn>(),
+                SchemaTypeName = SchemaTypeNames.ChartVisualizationSettingsType,
+                Open = new List<Dom.Visualizations.MeasureColumn>(),
+                Close = new List<Dom.Visualizations.MeasureColumn>(),
+                High = new List<Dom.Visualizations.MeasureColumn>(),
+                Low = new List<Dom.Visualizations.MeasureColumn>()
+            };
+
+            // Act
+            var actualJson = financialVSDataSpec.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
+        }
+    }
+}
