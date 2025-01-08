c722b57c5390fab569a4ef69458a8f3ae20328b9
Phat Vo <recca5p@gmail.com>
2025-01-08 17:02:38 +0700

    create fixture file

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/GaugeVisualizationSettingsFixture.cs
@@ -0,0 +1,10 @@
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class GaugeVisualizationSettingsFixture
+{
+}
