6f39f9dd1832c0a0470053bda819f514e4fbed6b
Viktor <VLe@infragistics.com>
2025-01-09 15:20:12 +0700

    Update UT

--- src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/TimeSeriesVisualizationDataSpecFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/VisualizationSpecs/TimeSeriesVisualizationDataSpecFixture.cs
@@ -24,7 +24,7 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
             // Arrange
             var expectedJson = """
                 {
-                  "_type": "ChartVisualizationSettingsType",
+                  "_type": "TimeSeriesVisualizationDataSpecType",
                   "Category": {
                     "_type": "DimensionColumnSpecType"
                   },
@@ -41,7 +41,6 @@ namespace Reveal.Sdk.Dom.Tests.Visualizations.VisualizationSpecs
                 AdHocFields = 13,
                 FormatVersion = 5,
                 Rows = new List<Dom.Visualizations.DimensionColumn>(),
-                SchemaTypeName = SchemaTypeNames.ChartVisualizationSettingsType,
                 Category = new Dom.Visualizations.DimensionColumn(),
                 Values = new List<Dom.Visualizations.MeasureColumn>()
             };
