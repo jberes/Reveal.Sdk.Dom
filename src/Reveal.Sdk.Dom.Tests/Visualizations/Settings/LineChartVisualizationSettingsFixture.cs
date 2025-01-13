e47d3adc76aaeb84b8a69254a9062cdb605a35f1
Phat Vo <recca5p@gmail.com>
2025-01-13 14:11:12 +0700

    Create LineChartVisualizationSettingsFixture.cs
    create UTs

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/LineChartVisualizationSettingsFixture.cs
@@ -0,0 +1,70 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.Settings;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class LineChartVisualizationSettingsFixture
+{
+    [Fact]
+    public void Constructor_FieldsHaveDefaultValues_WhenInstanceIsCreated()
+    {
+        // Act
+        var settings = new LineChartVisualizationSettings();
+
+        // Assert
+        Assert.True(settings.AutomaticLabelRotation);
+        Assert.Equal(RdashChartType.Line, settings.ChartType);
+        Assert.Equal(SchemaTypeNames.ChartVisualizationSettingsType, settings.SchemaTypeName);
+        Assert.True(settings.ShowLegend);
+        Assert.False(settings.ShowTotalsInTooltip);
+        Assert.Null(settings.StartColorIndex);
+        Assert.False(settings.SyncAxis);
+        Assert.Equal(default(TrendlineType), settings.Trendline);
+        Assert.Equal(VisualizationTypes.CHART, settings.VisualizationType);
+        Assert.False(settings.YAxisIsLogarithmic);
+        Assert.Null(settings.YAxisMaxValue);
+        Assert.Null(settings.YAxisMinValue);
+        Assert.Equal(1.0, settings.ZoomLevel);
+        Assert.Equal(1.0, settings.ZoomScaleHorizontal);
+        Assert.Equal(1.0, settings.ZoomScaleVertical);
+    }
+
+    [Fact]
+    public void ToJsonString_GeneratesCorrectJson_WhenSerialized()
+    {
+        // Arrange
+        var expectedJson =
+            """
+            {
+              "_type" : "ChartVisualizationSettingsType",
+              "ShowTotalsInTooltip" : false,
+              "TrendlineType" : "None",
+              "AutomaticLabelRotation" : true,
+              "SyncAxisVisibleRange" : false,
+              "ZoomScaleHorizontal" : 1.0,
+              "ZoomScaleVertical" : 1.0,
+              "LeftAxisLogarithmic" : false,
+              "LeftAxisMinValue" : null,
+              "LeftAxisMaxValue" : null,
+              "ShowLegends" : true,
+              "BrushOffsetIndex" : null,
+              "ChartType" : "Line",
+              "VisualizationType" : "CHART"
+            }
+            """;
+
+        var settings = new LineChartVisualizationSettings();
+
+        // Act
+        var actualJson = JsonConvert.SerializeObject(settings);
+        var expectedJObject = JObject.Parse(expectedJson);
+        var actualJObject = JObject.Parse(actualJson);
+
+        // Assert
+        Assert.Equal(expectedJObject, actualJObject);
+    }
+}
