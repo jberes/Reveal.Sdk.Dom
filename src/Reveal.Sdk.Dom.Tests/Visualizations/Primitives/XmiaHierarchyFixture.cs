e4a4ea3edca5997e926895ed252914087fd0dab1
Henry <hnguyen@infragistics.com>
2025-01-13 21:57:43 +0700

    add XmlaHierarchy test

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/XmiaHierarchyFixture.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class XmiaHierarchyFixture
+    {
+        [Fact]
+        public void Constructor_SetDefaultValues_WhenConstructed()
+        {
+            // Act
+            var instance = new XmlaHierarchy();
+
+            // Assert
+            Assert.Equal(SchemaTypeNames.XmlaHierarchyType, instance.SchemaTypeName);
+        }
+
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var instance = new XmlaHierarchy()
+            {
+                Origin = 1,
+                Description = "Description",
+                Cardinality = 1,
+                DisplayFolder = "DisplayFolder",
+                AllMember = "AllMember",
+                DefaultMember = "DefaultMember"
+            };
+
+            var expectedJson = """
+            {
+              "_type": "XmlaHierarchyType",
+              "Origin": 1,
+              "Description": "Description",
+              "Cardinality": 1,
+              "DisplayFolder": "DisplayFolder",
+              "AllMember": "AllMember",
+              "DefaultMember": "DefaultMember",
+              "DimensionType": "Regular",
+              "DrillDownElements": [],
+              "Sorting": "None",
+              "FieldSortingByLabel": false,
+              "FullyExpandedLevels": 0,
+              "ExpandedItems": [],
+              "DateAggregationType": "Year",
+              "DateFiscalYearStartMonth": 0,
+              "DrillDownMembers": []
+            }
+            """;
+
+            // Act
+            var actualJson = instance.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
+        }
+    }
+}
