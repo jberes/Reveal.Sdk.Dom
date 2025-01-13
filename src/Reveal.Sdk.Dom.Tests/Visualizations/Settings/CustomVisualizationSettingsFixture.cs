ed9327560b4e3d2eef6f58765377b7b9bd1783f1
Phat Vo <recca5p@gmail.com>
2025-01-13 13:48:37 +0700

    Create CustomVisualizationSettingsFixture.cs
    create UTs

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/CustomVisualizationSettingsFixture.cs
@@ -0,0 +1,65 @@
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations.Settings;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class CustomVisualizationSettingsFixture
+{
+    [Fact]
+    public void Constructor_FieldsHaveDefaultValues_WhenInstanceIsCreated()
+    {
+        // Act
+        var settings = new CustomVisualizationSettings();
+
+        // Assert
+        Assert.Equal(SchemaTypeNames.DiyVisualizationSettingsType, settings.SchemaTypeName);
+        Assert.Equal(VisualizationTypes.JS_EXTENSION, settings.VisualizationType);
+        Assert.Null(settings.Title);
+        Assert.Null(settings.Url);
+    }
+
+    [Fact]
+    public void Title_SetAndGet_UpdatesCorrectly()
+    {
+        // Arrange
+        var settings = new CustomVisualizationSettings();
+        var expectedTitle = "Custom Title";
+
+        // Act
+        settings.Title = expectedTitle;
+
+        // Assert
+        Assert.Equal(expectedTitle, settings.Title);
+    }
+
+    [Fact]
+    public void ToJsonString_GeneratesCorrectJson_WhenSerialized()
+    {
+        // Arrange
+        var expectedJson =
+            """
+            {
+              "_type": "DiyVisualizationSettingsType",
+              "VisualizationType": "JS_EXTENSION",
+              "Title": "Custom Visualization",
+              "Url": "http://dl.infragistics.com/reportplus/reveal/samples/Samples.xlsx"
+            }
+            """;
+
+        var settings = new CustomVisualizationSettings
+        {
+            Title = "Custom Visualization",
+            Url = "http://dl.infragistics.com/reportplus/reveal/samples/Samples.xlsx"
+        };
+
+        // Act
+        var actualJson = settings.ToJsonString();
+        var expectedJObject = JObject.Parse(expectedJson);
+        var actualJObject = JObject.Parse(actualJson);
+
+        // Assert
+        Assert.Equal(expectedJObject, actualJObject);
+    }
+}
