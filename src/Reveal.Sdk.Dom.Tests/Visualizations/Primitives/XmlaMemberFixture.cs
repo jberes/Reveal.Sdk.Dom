4abeb96fa3e1c337f1366f785143d35c6edf3e55
Henry <hnguyen@infragistics.com>
2025-01-13 22:17:03 +0700

    Add XmlaMember test

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Primitives/XmlaMemberFixture.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Visualizations;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Primitives
+{
+    public class XmlaMemberFixture
+    {
+        [Fact]
+        public void ToJsonString_CreateCorrectJsonString_WithoutCondition()
+        {
+            // Arrange
+            var instance = new XmlaMember()
+            {
+                UniqueName = "UniqueName",
+                Caption = "Caption"
+            };
+
+            var expectedJson = """
+            {
+              "UniqueName": "UniqueName",
+              "Caption": "Caption"
+            }
+            """;
+
+            // Act
+            var actualJson = instance.ToJsonString();
+            var expectedJObject = JObject.Parse(expectedJson);
+            var actualJObject = JObject.Parse(actualJson);
+
+            // Assert
+            Assert.Equal(expectedJObject, actualJObject);
+        }
+    }
+}
