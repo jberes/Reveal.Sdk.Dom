c2f514379594ed5f461f94378947f633ef96082d
Phat Vo <recca5p@gmail.com>
2025-01-15 16:46:16 +0700

    Update LineChartVisualizationSettingsFixture.cs

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/LineChartVisualizationSettingsFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/LineChartVisualizationSettingsFixture.cs
@@ -48,10 +48,7 @@ public class LineChartVisualizationSettingsFixture
               "ZoomScaleHorizontal" : 1.0,
               "ZoomScaleVertical" : 1.0,
               "LeftAxisLogarithmic" : false,
-              "LeftAxisMinValue" : null,
-              "LeftAxisMaxValue" : null,
               "ShowLegends" : true,
-              "BrushOffsetIndex" : null,
               "ChartType" : "Line",
               "VisualizationType" : "CHART"
             }
@@ -60,7 +57,7 @@ public class LineChartVisualizationSettingsFixture
         var settings = new LineChartVisualizationSettings();
 
         // Act
-        var actualJson = JsonConvert.SerializeObject(settings);
+        var actualJson = settings.ToJsonString();
         var expectedJObject = JObject.Parse(expectedJson);
         var actualJObject = JObject.Parse(actualJson);
 
