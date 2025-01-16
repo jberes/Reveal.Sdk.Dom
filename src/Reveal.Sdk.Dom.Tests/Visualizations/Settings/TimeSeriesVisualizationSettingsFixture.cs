5657432338b945b20b2e067380f20fa16a7fcdfa
Phat Vo <recca5p@gmail.com>
2025-01-16 22:34:43 +0700

    clean up

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/TimeSeriesVisualizationSettingsFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/TimeSeriesVisualizationSettingsFixture.cs
@@ -16,21 +16,7 @@ public class TimeSeriesVisualizationSettingsFixture
         var settings = new TimeSeriesVisualizationSettings();
 
         // Assert
-        Assert.True(settings.AutomaticLabelRotation);
         Assert.Equal(RdashChartType.TimeSeries, settings.ChartType);
-        Assert.Equal(SchemaTypeNames.ChartVisualizationSettingsType, settings.SchemaTypeName);
-        Assert.True(settings.ShowLegend);
-        Assert.False(settings.ShowTotalsInTooltip);
-        Assert.Null(settings.StartColorIndex);
-        Assert.False(settings.SyncAxis);
-        Assert.Equal(default(TrendlineType), settings.Trendline);
-        Assert.Equal(VisualizationTypes.CHART, settings.VisualizationType);
-        Assert.False(settings.YAxisIsLogarithmic);
-        Assert.Null(settings.YAxisMaxValue);
-        Assert.Null(settings.YAxisMinValue);
-        Assert.Equal(1.0, settings.ZoomLevel);
-        Assert.Equal(1.0, settings.ZoomScaleHorizontal);
-        Assert.Equal(1.0, settings.ZoomScaleVertical);
     }
 
     [Fact]
