e2e5be09fbd266cd823b270e742db967e8eea7a3
Phat Vo <recca5p@gmail.com>
2025-01-16 22:23:06 +0700

    Create StackedBarChartVisualizationSettingsFixture.cs
    create UTs

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/StackedBarChartVisualizationSettingsFixture.cs
@@ -0,0 +1,55 @@
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.Settings;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class StackedBarChartVisualizationSettingsFixture
+{
+    [Fact]
+    public void Constructor_FieldsHaveDefaultValues_WhenInstanceIsCreated()
+    {
+        // Act
+        var settings = new StackedBarChartVisualizationSettings();
+
+        // Assert
+        Assert.Equal(RdashChartType.StackedBar, settings.ChartType);
+    }
+
+    [Fact]
+    public void ToJsonString_GeneratesCorrectJson_WhenSerialized()
+    {
+        // Arrange
+        var expectedJson =
+            """
+            {
+              "_type" : "ChartVisualizationSettingsType",
+              "IsPercentageDistributed" : true,
+              "ShowTotalsInTooltip" : true,
+              "AutomaticLabelRotation" : true,
+              "SyncAxisVisibleRange" : false,
+              "ZoomScaleHorizontal" : 1.0,
+              "ZoomScaleVertical" : 1.0,
+              "LeftAxisLogarithmic" : false,
+              "ShowLegends" : true,
+              "ChartType" : "StackedBar",
+              "VisualizationType" : "CHART"
+            }
+            """;
+
+        var settings = new StackedBarChartVisualizationSettings
+        {
+            IsPercentageDistributed = true,
+            ShowTotalsInTooltip = true
+        };
+
+        // Act
+        var actualJson = settings.ToJsonString();
+        var expectedJObject = JObject.Parse(expectedJson);
+        var actualJObject = JObject.Parse(actualJson);
+
+        // Assert
+        Assert.Equal(expectedJObject, actualJObject);
+    }
+}
