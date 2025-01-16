8d3d58ceffec3793e1c91070783393baeeb0ebee
Phat Vo <recca5p@gmail.com>
2025-01-16 09:34:58 +0700

    Create RadialVisualizationSettingsFixture.cs
    create UTs

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/RadialVisualizationSettingsFixture.cs
@@ -0,0 +1,56 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.Settings;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class RadialVisualizationSettingsFixture
+{
+    [Fact]
+    public void Constructor_FieldsHaveDefaultValues_WhenInstanceIsCreated()
+    {
+        // Act
+        var settings = new RadialVisualizationSettings();
+
+        // Assert
+        Assert.Equal(RdashChartType.RadialLines, settings.ChartType);
+        Assert.Equal(SchemaTypeNames.ChartVisualizationSettingsType, settings.SchemaTypeName);
+        Assert.Equal(VisualizationTypes.CHART, settings.VisualizationType);
+        Assert.Equal(TrendlineType.None, settings.Trendline);
+        Assert.True(settings.ShowLegend);
+        Assert.Null(settings.StartColorIndex);
+    }
+
+    [Fact]
+    public void ToJsonString_GeneratesCorrectJson_WhenSerialized()
+    {
+        // Arrange
+        var expectedJson =
+            """
+            {
+              "_type" : "ChartVisualizationSettingsType",
+              "TrendlineType" : "LinearFit",
+              "ShowLegends" : true,
+              "BrushOffsetIndex" : null,
+              "ChartType" : "RadialLines",
+              "VisualizationType" : "CHART"
+            }
+            """;
+
+        var settings = new RadialVisualizationSettings
+        {
+            Trendline = TrendlineType.LinearFit
+        };
+
+        // Act
+        var actualJson = JsonConvert.SerializeObject(settings, Formatting.Indented);
+        var expectedJObject = JObject.Parse(expectedJson);
+        var actualJObject = JObject.Parse(actualJson);
+
+        // Assert
+        Assert.Equal(expectedJObject, actualJObject);
+    }
+}
