bef131561ebd5fd8f1e7d7cfab2f4fe990bc3bc2
Phat Vo <recca5p@gmail.com>
2025-01-16 22:39:03 +0700

    clean up

--- src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/BubbleVisualizationSettingsFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/BubbleVisualizationSettingsFixture.cs
@@ -17,15 +17,6 @@ public class BubbleVisualizationSettingsFixture
 
         // Assert
         Assert.Equal(RdashChartType.Bubble, settings.ChartType);
-        Assert.Equal(SchemaTypeNames.ChartVisualizationSettingsType, settings.SchemaTypeName);
-        Assert.Equal(VisualizationTypes.CHART, settings.VisualizationType);
-        Assert.False(settings.XAxisIsLogarithmic);
-        Assert.False(settings.YAxisIsLogarithmic);
-        Assert.Null(settings.XAxisMinValue);
-        Assert.Null(settings.XAxisMaxValue);
-        Assert.Null(settings.YAxisMinValue);
-        Assert.Null(settings.YAxisMaxValue);
-        Assert.True(settings.ShowLegend);
     }
     
     [Fact]
