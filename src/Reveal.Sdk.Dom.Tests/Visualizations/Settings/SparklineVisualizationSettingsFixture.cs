707592fba2c98eb26ced9326fb5330ca94a8b7e7
Phat Vo <recca5p@gmail.com>
2025-01-16 22:19:24 +0700

    Create SparklineVisualizationSettingsFixture.cs
    create UTs

---
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/Settings/SparklineVisualizationSettingsFixture.cs
@@ -0,0 +1,128 @@
+using Newtonsoft.Json.Linq;
+using Reveal.Sdk.Dom.Core.Constants;
+using Reveal.Sdk.Dom.Visualizations;
+using Reveal.Sdk.Dom.Visualizations.Settings;
+using Reveal.Sdk.Dom.Visualizations.VisualizationSpecs;
+using Xunit;
+
+namespace Reveal.Sdk.Dom.Tests.Visualizations.Settings;
+
+public class SparklineVisualizationSettingsFixture
+{
+    [Fact]
+    public void Constructor_FieldsHaveDefaultValues_WhenInstanceIsCreated()
+    {
+        // Act
+        var settings = new SparklineVisualizationSettings();
+
+        // Assert
+        Assert.Equal(SchemaTypeNames.SparklineVisualizationSettingsType, settings.SchemaTypeName);
+        Assert.Equal(VisualizationTypes.SPARKLINE, settings.VisualizationType);
+        Assert.Equal(SparklineChartType.Line, settings.ChartType);
+        Assert.True(settings.ShowLastTwoValues);
+        Assert.True(settings.ShowDifference);
+        Assert.False(settings.PositiveIsRed);
+    }
+
+    [Fact]
+    public void ToJsonString_GeneratesCorrectJson_WhenSerialized()
+    {
+        // Arrange
+        var expectedJson =
+            """
+            {
+              "_type" : "SparklineVisualizationSettingsType",
+              "ChartType" : "Area",
+              "PositiveIsRed" : true,
+              "ShowLastTwoValues" : false,
+              "ShowDifference" : false,
+              "FontSize" : "Small",
+              "Style" : {
+                "FixedLeftColumns" : false,
+                "TextAlignment" : "Inherit",
+                "NumericAlignment" : "Inherit",
+                "DateAlignment" : "Inherit"
+              },
+              "VisualizationType" : "SPARKLINE"
+            }
+            """;
+
+        var settings = new SparklineVisualizationSettings
+        {
+            ChartType = SparklineChartType.Area,
+            ShowLastTwoValues = false,
+            ShowDifference = false,
+            PositiveIsRed = true
+        };
+
+        // Act
+        var actualJson = settings.ToJsonString();
+        var expectedJObject = JObject.Parse(expectedJson);
+        var actualJObject = JObject.Parse(actualJson);
+
+        // Assert
+        Assert.Equal(expectedJObject, actualJObject);
+    }
+    
+    [Theory]
+    [InlineData(IndicatorVisualizationType.LastDays, SparklineAggregationType.Days)]
+    [InlineData(IndicatorVisualizationType.LastMonths, SparklineAggregationType.Months)]
+    [InlineData(IndicatorVisualizationType.LastYears, SparklineAggregationType.Years)]
+    internal void AggregationType_SetAndGet_ConvertsValuesCorrectly(
+        IndicatorVisualizationType indicatorType,
+        SparklineAggregationType expectedAggregationType)
+    {
+        // Arrange
+        var settings = new SparklineVisualizationSettings
+        {
+            _visualizationDataSpec = new SparklineVisualizationDataSpec
+            {
+                IndicatorType = indicatorType
+            }
+        };
+
+        // Act
+        var aggregationType = settings.AggregationType;
+
+        // Assert
+        Assert.Equal(expectedAggregationType, aggregationType);
+    }
+
+    [Theory]
+    [InlineData(SparklineAggregationType.Days, IndicatorVisualizationType.LastDays)]
+    [InlineData(SparklineAggregationType.Months, IndicatorVisualizationType.LastMonths)]
+    [InlineData(SparklineAggregationType.Years, IndicatorVisualizationType.LastYears)]
+    internal void AggregationType_SetAndGet_UpdatesVisualizationDataSpec(
+        SparklineAggregationType aggregationType,
+        IndicatorVisualizationType expectedIndicatorType)
+    {
+        // Arrange
+        var settings = new SparklineVisualizationSettings
+        {
+            _visualizationDataSpec = new SparklineVisualizationDataSpec()
+        };
+
+        // Act
+        settings.AggregationType = aggregationType;
+
+        // Assert
+        Assert.Equal(expectedIndicatorType, settings._visualizationDataSpec.IndicatorType);
+    }
+
+    [Fact]
+    public void NumberOfPeriods_SetAndGet_UpdatesVisualizationDataSpec()
+    {
+        // Arrange
+        var settings = new SparklineVisualizationSettings
+        {
+            _visualizationDataSpec = new SparklineVisualizationDataSpec()
+        };
+
+        // Act
+        settings.NumberOfPeriods = 10;
+
+        // Assert
+        Assert.Equal(10, settings.NumberOfPeriods);
+        Assert.Equal(10, settings._visualizationDataSpec.NumberOfPeriods);
+    }
+}
