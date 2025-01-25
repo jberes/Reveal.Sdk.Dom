80809a4580b149d51c4e07e032b01a0f1d8364da
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2025-01-24 17:50:21 -0700

    improved import logic

---
+++ src/Reveal.Sdk.Dom/Primitives/ImportOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Reveal.Sdk.Dom
+{
+    public class ImportOptions
+    {
+        public bool IncludeDashboardFilters { get; set; }
+        public bool IncludeVisualizationFilters { get; set; }
+    }
+}
