206b2332b69f4a7f8013fc0cf08c568f1716d05b
Phat Vo <recca5p@gmail.com>
2025-02-07 10:27:57 +0700

    fix cmt

--- src/Reveal.Sdk.Dom.Tests/Visualizations/TreeMapVisualizationFixture.cs
+++ src/Reveal.Sdk.Dom.Tests/Visualizations/TreeMapVisualizationFixture.cs
@@ -69,6 +69,7 @@ public class TreeMapVisualizationFixture
 
         // Assert
         Assert.Equal(expectedLabels, labels);
+        Assert.Equal(expectedLabels, visualization.VisualizationDataSpec.Rows);
     }
 
     [Fact]
@@ -84,6 +85,7 @@ public class TreeMapVisualizationFixture
 
         // Assert
         Assert.Equal(expectedValues, values);
+        Assert.Equal(expectedValues, visualization.VisualizationDataSpec.Value);
     }
 
     [Fact]
