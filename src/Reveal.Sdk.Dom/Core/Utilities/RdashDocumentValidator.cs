7d5dc05e7f6f4479928321ec10b818a4534cae2a
Sebastian Rodriguez <srodriguez@infragistics.com>
2025-02-25 17:43:32 -0300

    Change the exception thrown on ValidateManuallyAddedDataSourceItem to a warning, as some special datasources (e.g.: '__EXCEL'), would erroneously trigger the exception.

--- src/Reveal.Sdk.Dom/Core/Utilities/RdashDocumentValidator.cs
+++ src/Reveal.Sdk.Dom/Core/Utilities/RdashDocumentValidator.cs
@@ -3,6 +3,7 @@ using Reveal.Sdk.Dom.Filters;
 using Reveal.Sdk.Dom.Visualizations;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Reveal.Sdk.Dom.Core.Utilities
@@ -79,13 +80,13 @@ namespace Reveal.Sdk.Dom.Core.Utilities
         {
             var ds = document.DataSources?.FirstOrDefault(x => x.Id == dsi.DataSourceId);
             if (ds == null)
-                throw new Exception($"Data source with id {dsi.DataSourceId} not found in the RdashDocument.DataSources collection.");
+                Debug.WriteLine($"Warning: Data source with id {dsi.DataSourceId} not found in the RdashDocument.DataSources collection.","warn");
 
             if (dsi.ResourceItem != null)
             {
                 var rds = document.DataSources?.FirstOrDefault(x => x.Id == dsi.ResourceItem.DataSourceId);
                 if (rds == null)
-                    throw new Exception($"ResourceItem with Data source id {dsi.ResourceItem.DataSourceId} not found in the RdashDocument.DataSources collection.");
+                Debug.WriteLine($"Warning: ResourceItem with Data source id {dsi.ResourceItem.DataSourceId} not found in the RdashDocument.DataSources collection.", "warn");
             }
         }
 
