6bce64799560134709e8e051401c8b74143523b5
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2025-03-26 16:08:42 -0600

    added comments

--- src/Reveal.Sdk.Dom/Visualizations/Settings/BarChartVisualizationSettings.cs
+++ src/Reveal.Sdk.Dom/Visualizations/Settings/BarChartVisualizationSettings.cs
@@ -9,10 +9,16 @@ namespace Reveal.Sdk.Dom.Visualizations.Settings
             ChartType = RdashChartType.Bar;
         }
 
-        [JsonProperty("CategoryAxisOverlap")]
-        public double Overlap { get; set; } = -0.2;
-
+        /// <summary>
+        /// Gets or sets the gap between the bars in the chart. The value is a percentage of the bar width.
+        /// </summary>
         [JsonProperty("CategoryAxisGap")]
         public double Gap { get; set; } = 0.4;
+
+        /// <summary>
+        /// Gets or sets the overlap between the bars in the chart. The value is a percentage of the bar width.
+        /// </summary>
+        [JsonProperty("CategoryAxisOverlap")]
+        public double Overlap { get; set; } = -0.2;
     }
 }
